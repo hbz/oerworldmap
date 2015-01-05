0f7082b5ea44d98b6805adcf8f954d3174b77da9
Philipp v. Böselager <boeselager@hbz-nrw.de>
2015-01-05 17:47:40 +0100

    Add email validation to the demo application.

--- app/controllers/UserIndex.java
+++ app/controllers/UserIndex.java
@@ -1,10 +1,14 @@
 package controllers;
 
-import play.*;
+import java.util.ArrayList;
+import java.util.List;
+
 import play.mvc.*;
-import play.data.*;
-import play.libs.Json;
+import play.data.Form;
+import play.data.validation.ValidationError;
 import models.User;
+
+import org.apache.commons.validator.routines.EmailValidator;
 
 public class UserIndex extends Controller {
 
@@ -13,12 +17,27 @@ public class UserIndex extends Controller {
     public static Result get() {
         return ok(views.html.UserIndex.index.render(registrationForm));
     }
-
+    
     public static Result post() {
-        DynamicForm requestData = Form.form().bindFromRequest();
-        String email = requestData.get("email");
+        String email = Form.form().bindFromRequest().get("email");
+        List<ValidationError> errors = validate(email);
+        if (errors != null && !errors.isEmpty()){
+            return badRequest("E-mail error");				// TODO: to be specified
+        }
         return ok(views.html.UserIndex.registered.render(email));
     }
 
+    
+    private static List<ValidationError> validate(String aEmail) {
+	
+	List<ValidationError> errors = new ArrayList<ValidationError>();
+	
+	if (User.byEmail(aEmail) != null) {
+	    errors.add(new ValidationError("email", "This e-mail is already registered."));
+	}
+	else if(! EmailValidator.getInstance().isValid(aEmail)){
+	    errors.add(new ValidationError("email", "This is not a valid e-mail adress."));
+	}
+	return errors.isEmpty() ? null : errors;
+    }
 }
-
--- app/models/User.java
+++ app/models/User.java
@@ -1,7 +1,29 @@
 package models;
 
+import javax.annotation.Nonnull;
+
 public class User {
 
   public String email;
 
+  // the default constructor is explicitly needed to make
+  // User be processable in Play forms since we have invented
+  // another constructor.
+  public User(){}
+  
+  
+  public User(@Nonnull String aEmail){
+      email = aEmail;
+  }
+  
+  public static User byEmail(@Nonnull String aEmail){
+      
+      // TODO: still fake; match against elasticsearch
+      if (aEmail.startsWith("exist")){
+	  return new User(aEmail);
+      }
+      else {
+	  return null;
+      }
+  }
 }
