1c5ea3403f48f05e949dc635d88a507497e32165
Felix Ostrowski <felix.ostrowski@googlemail.com>
2015-01-06 14:55:53 +0100

    Use annotation based validation for email addresses

--- app/controllers/UserIndex.java
+++ app/controllers/UserIndex.java
@@ -1,43 +1,26 @@
 package controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import play.*;
 import play.mvc.*;
-import play.data.Form;
-import play.data.validation.ValidationError;
+import play.data.*;
+import play.libs.Json;
 import models.User;
-
-import org.apache.commons.validator.routines.EmailValidator;
 
 public class UserIndex extends Controller {
 
-    final static Form<User> registrationForm = Form.form(User.class);
-
     public static Result get() {
-        return ok(views.html.UserIndex.index.render(registrationForm));
-    }
-    
-    public static Result post() {
-        String email = Form.form().bindFromRequest().get("email");
-        List<ValidationError> errors = validate(email);
-        if (errors != null && !errors.isEmpty()){
-            return badRequest("E-mail error");				// TODO: to be specified
-        }
-        return ok(views.html.UserIndex.registered.render(email));
+        return ok(views.html.UserIndex.index.render(Form.form(User.class)));
     }
 
-    
-    private static List<ValidationError> validate(String aEmail) {
-	
-	List<ValidationError> errors = new ArrayList<ValidationError>();
-	
-	if (User.byEmail(aEmail) != null) {
-	    errors.add(new ValidationError("email", "This e-mail is already registered."));
-	}
-	else if(! EmailValidator.getInstance().isValid(aEmail)){
-	    errors.add(new ValidationError("email", "This is not a valid e-mail adress."));
-	}
-	return errors.isEmpty() ? null : errors;
+    public static Result post() {
+        Form<User> requestData = Form.form(User.class).bindFromRequest();
+        if (requestData.hasErrors()) {
+            return badRequest(views.html.UserIndex.index.render(requestData));
+        } else {
+            User user = requestData.get();
+            return ok(views.html.UserIndex.registered.render(user.email));
+        }
     }
+
 }
+
--- app/models/User.java
+++ app/models/User.java
@@ -1,29 +1,11 @@
 package models;
 
-import javax.annotation.Nonnull;
+import play.data.validation.Constraints;
 
 public class User {
 
+  @Constraints.Email
+  @Constraints.Required
   public String email;
 
-  // the default constructor is explicitly needed to make
-  // User be processable in Play forms since we have invented
-  // another constructor.
-  public User(){}
-  
-  
-  public User(@Nonnull String aEmail){
-      email = aEmail;
-  }
-  
-  public static User byEmail(@Nonnull String aEmail){
-      
-      // TODO: still fake; match against elasticsearch
-      if (aEmail.startsWith("exist")){
-	  return new User(aEmail);
-      }
-      else {
-	  return null;
-      }
-  }
 }
