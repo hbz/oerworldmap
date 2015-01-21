0cb6aaf7fa03441bb37a91fe42133c7a1da2af68
Philipp v. Böselager <boeselager@hbz-nrw.de>
2015-01-21 11:45:25 +0100

    Provide ApplicationTest with running(testServer(PORT, fakeApp...)

--- test/oerworldmap/ApplicationTest.java
+++ test/oerworldmap/ApplicationTest.java
@@ -1,24 +1,27 @@
 package oerworldmap;
 
 import static org.fest.assertions.Assertions.assertThat;
-import static play.test.Helpers.GET;
-import static play.test.Helpers.contentType;
+import static play.test.Helpers.HTMLUNIT;
 import static play.test.Helpers.fakeApplication;
-import static play.test.Helpers.fakeRequest;
-import static play.test.Helpers.route;
+import static play.test.Helpers.inMemoryDatabase;
 import static play.test.Helpers.running;
+import static play.test.Helpers.testServer;
 
 import org.junit.Test;
 
-import play.mvc.Result;
+import play.libs.F.Callback;
+import play.test.TestBrowser;
 
 public class ApplicationTest {
   @Test
   public void runningLandingPage() {
-    running(fakeApplication(), () -> {
-      Result result = route(fakeRequest(GET, "/user"));
-      assertThat(result).isNotNull();
-      assertThat(contentType(result)).isEqualTo("text/html");
-    });
+    running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT,
+        new Callback<TestBrowser>() {
+          @Override
+          public void invoke(TestBrowser browser) {
+            browser.goTo("http://localhost:3333/user");
+            assertThat(browser.pageSource().contains("Registration"));
+          }
+        });
   }
 }
