f42d1b0f2f0ea861ea9ce6cda3ec9ed4f645fbad
Felix Ostrowski <felix.ostrowski@googlemail.com>
2015-01-28 19:18:32 +0100

    Store user data in ElasticsearchRepository

--- app/controllers/UserIndex.java
+++ app/controllers/UserIndex.java
@@ -1,10 +1,22 @@
 package controllers;
 
+import models.Resource;
 import models.User;
+import play.data.DynamicForm;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import services.ElasticsearchClient;
+import services.ElasticsearchConfig;
+import services.ElasticsearchRepository;
+import services.ResourceRepository;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.client.Client;
+
+
+import java.io.IOException;
+
+import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
 public class UserIndex extends Controller {
 
@@ -12,19 +24,20 @@ public class UserIndex extends Controller {
     return ok(views.html.UserIndex.index.render(Form.form(User.class)));
   }
 
-  public static Result post() {
-    Form<User> requestData = Form.form(User.class).bindFromRequest();
+  public static Result post() throws IOException {
+    DynamicForm requestData = Form.form().bindFromRequest();
     if (requestData.hasErrors()) {
-      return badRequest(views.html.UserIndex.index.render(requestData));
+      return badRequest(views.html.UserIndex.index.render(Form.form(User.class)));
     } else {
-      User user = requestData.get();
-      System.err.println(ElasticsearchClient.getClient().settings().getAsMap());
-      ElasticsearchClient.getClient()
-          .prepareIndex(ElasticsearchClient.getIndex(), ElasticsearchClient.getType())
-          .setSource(user.toString())
-          .execute().actionGet();
-          
-      return ok(views.html.UserIndex.registered.render(user.email));
+      Resource user = new Resource("person");
+      user.set("email", requestData.get("email"));
+      ElasticsearchConfig esConfig = new ElasticsearchConfig();
+      Node mNode = nodeBuilder().settings(esConfig.getClientSettings()).node();
+      Client mClient = mNode.client();
+      ElasticsearchClient mElasticsearchClient = new ElasticsearchClient(mClient);
+      ResourceRepository resourceRepository = new ElasticsearchRepository(mElasticsearchClient);
+      resourceRepository.addResource(user);
+      return ok(views.html.UserIndex.registered.render((String) user.get("email")));
     }
   }
   
