69ec497f84b00ec0f8fb27b63c8919e1a7ef0c4b
Philipp v. Böselager <boeselager@hbz-nrw.de>
2015-02-24 08:43:58 +0100

    Introduce organization json-based datamodel and unit test

--- build.sbt
+++ build.sbt
@@ -9,5 +9,7 @@ libraryDependencies ++= Seq(
   javaWs,
   "org.elasticsearch" % "elasticsearch" % "1.3.6",
   "commons-validator" % "commons-validator" % "1.4.0",
-  "org.apache.commons" % "commons-email" % "1.3.3"
+  "org.apache.commons" % "commons-email" % "1.3.3",
+  "com.github.fge" % "jackson-coreutils" % "1.8",
+  "com.github.fge" % "json-schema-validator" % "2.1.7"
 )
---
+++ test/models/OrganizationTest.java
@@ -0,0 +1,39 @@
+package models;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonschema.exceptions.ProcessingException;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
+import com.github.fge.jsonschema.report.ProcessingReport;
+
+public class OrganizationTest {
+
+  private static JsonSchema organizationSchema;
+  private static JsonNode organizationInstance;
+
+  @BeforeClass
+  public static void testConstructorWithoutId() throws JsonProcessingException, ProcessingException,
+      IOException {
+
+    organizationSchema = JsonSchemaFactory.byDefault().getJsonSchema(
+        new ObjectMapper().readTree(Paths.get("public/json/schema/organization.json").toFile()));
+    organizationInstance = new ObjectMapper().readTree(Files.readAllBytes(Paths
+        .get("public/json/ld/organization.jsonld")));
+  }
+
+  @Test
+  public void testValidate() throws ProcessingException {
+    ProcessingReport report = organizationSchema.validate(organizationInstance);
+    Assert.assertTrue(report.isSuccess());
+  }
+}
