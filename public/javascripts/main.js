c440d2a24a59f454c08edbe8d1d786a0b16040dd
joh <git@sights.de>
2015-02-25 18:38:45 +0100

    extended post preview to a longer snippet

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,13 @@
+String.prototype.cutOff = function(x) {
+  //trim the string to the maximum length
+  var trimmedString = this.substr(0, x);
+  
+  //re-trim if we are in the middle of a word
+  trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")));
+  
+  return trimmedString + " ...";
+};
+
 // --- feed ---
 
 google.load("feeds", "1");
@@ -6,34 +16,13 @@ function initialize() {
   var feed = new google.feeds.Feed("https://oerworldmap.wordpress.com/feed/");
   feed.load(function(result) {
     if (!result.error) {
+      result.feed.entries[0].contentSnippet300 = $( result.feed.entries[0].content ).text().cutOff(300);        
       console.log(result.feed.entries[0]);
-      
       
       $.get('/assets/mustache/LandingPage/blog-post-preview.mustache', function(template) {
         var rendered = Mustache.render(template, {post: result.feed.entries[0]});
         $('#blog-link').prepend(rendered);
       });
-      
-/*
-      $('#blog-link').prepend(
-        '<div class="blog-post-preview blank-line" id="blog-latest-post-preview">' +
-          '<p>' + 
-            '<strong><a href="' + result.feed.entries[0].link + '">Latest Post: ' + result.feed.entries[0].title + '</a></strong><br/>' +
-            result.feed.entries[0].contentSnippet +
-          '</p>' +
-        '</div>'
-      );
-*/
-      
-/*
-      var container = document.getElementById("blog");
-      for (var i = 0; i < result.feed.entries.length; i++) {
-        var entry = result.feed.entries[i];
-        var div = document.createElement("div");
-        div.appendChild(document.createTextNode(entry.title));
-        container.appendChild(div);
-      }
-*/
 
     }
   });
