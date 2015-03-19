52d095dbf3fcd1a324ad0de032e1ef30c9e8f4ef
Philipp v. Böselager <boeselager@hbz-nrw.de>
2015-03-19 16:23:08 +0100

    Minor clode cleanup

---
+++ app/helpers/FilesConfig.java
@@ -0,0 +1,14 @@
+package helpers;
+
+
+public class FilesConfig {
+  
+  public static String getSchema(){
+    return "public/json/schema.json";
+  }
+  
+  public static String getOrganizationJsonld(){
+    return "public/json/ld/organization.jsonld";
+  }
+
+}
