6abdf590ae19d3a2060f54cd889e98f3efa3dd2f
joh <git@sights.de>
2015-04-15 20:34:49 +0200

    first mockup for column layout

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,7 @@
+// --- set js class on <html>
+
 document.documentElement.className = 'js';
+
 
 // --- helpers ---
 
@@ -19,199 +22,198 @@ String.prototype.cutOff = function(x) {
 };
 
 
-// --- hijax helper ---
+// --- hijax ---
 
-function hijax(element) {
+hijax = {
+  
+  init : function() {
+    hijax.hijax($('body'));
+  },
 
-  $('a.hijax.transclude', element).each(function() {
-    var a = $(this);
-    $.get(a.attr('href'))
-      .done(function(data) {
-        a.replaceWith(hijax(body(data)));
-        $('input, textarea').placeholder();
-      })
-      .fail(function(jqXHR) {
-        a.replaceWith(hijax(body(jqXHR.responseText)));
-      });
-  });
-
-  $('form', element).submit(function() {
-    var form = $(this);
-    var loading_indicator = $(this).find('button[type="submit"] .loading-indicator');
-    var action = form.attr('action');
-    var method = form.attr('method');
-    loading_indicator.show();
-    $.ajax({
-      type: method,
-      url: action,
-      data: form.serialize()
-    }).done(function(data, textStatus, xhr) {
-      var ct = xhr.getResponseHeader("content-type") || "";
-      if (ct.indexOf('html') > -1) {
-        form.replaceWith(hijax(body(data)));
-       } else {
-        form.replaceWith("<pre>" + data + "</pre>");
-       }
-      loading_indicator.hide();
-    }).fail(function(jqXHR) {
-      form.replaceWith(hijax(body(jqXHR.responseText)));
-      loading_indicator.hide();
+  hijax : function(element) {
+  
+    $('a.hijax.transclude', element).each(function() {
+      var a = $(this);
+      
+      $.get(a.attr('href'))
+        .done(function(data) {
+          a.replaceWith(hijax.hijax(hijax.extractBody(data)));
+          $('input, textarea').placeholder();
+        })
+        .fail(function(jqXHR) {
+          a.replaceWith(hijax.hijax(hijax.extractBody(jqXHR.responseText)));
+        });
     });
-    return false;
-  });
-
-  return element;
-
-}
-
-function body(data) {
-  return $(data.match(/<\s*body.*>[\s\S]*<\s*\/body\s*>/ig).join(""))
-}
-
-
-$(document).ready(function(){
   
-  // --- placeholder polyfill ---
-  
-  $('input, textarea').placeholder();
-
-  // --- map ---
-
-  var table = $('table[about="#users-by-country"]'),
-      map = $('#worldmap'),
-      json = JSON.parse(table.find('script').html()),
-      data = {};
-
-  for (i in json.entries) {
-    data[json.entries[i].key.toUpperCase()] = json.entries[i].value;
-  }
-
-  if(false) {
-    data = {
-      "DE" : 15,
-      "CH" : 4,
-      "AT" : 6,
-      "GB" : 12,
-      "FR" : 9,
-      "ES" : 5,
-      "US" : 9,
-      "PL" : 2,
-      "BF" : 1,
-      "NO" : 5,
-      "CN" : 6,
-      "ID" : 4,
-      "GH" : 4,
-      "IR" : 5,
-      "BR" : 7,
-      "CD" : 5,
-      "KZ" : 9,
-      "RU" : 2,
-      "RO" : 4,
-      "DZ" : 3,
-      "CA" : 2
-    };
-  }
-
-  map.vectorMap({
-    backgroundColor: '#0c75bf',
-    zoomButtons: false,
-    zoomOnScroll: false,
-    series: {
-      regions: [{
-        values: data,
-        scale: ['#cfdfba', '#a1cd3f'],
-        normalizeFunction: 'linear'
-      }]
-    },
-    onRegionTipShow: function(e, el, code){
-      var country_champion = false;
-      var users_registered = false;
-
-      if(
-        $('ul[about="#country-champions"] li[data-country-code="' + code + '"]').length
-      ) {
-        country_champion = true;
-      }
-
-      if(
-        typeof data[code] != 'undefined'
-      ) {
-        users_registered = true;
-      }
-
-      el.html(
-        (
-          users_registered
-          ?
-          '<i class="fa fa-fw fa-user"></i> <strong>' + data[code] + '</strong> users counted for ' + el.html() + (
-            $('div.register form').length
-            ?
-            ' (Click to be the next ...)<br>'
-            :
-            ''
-          )
-          :
-          '<i class="fa fa-fw fa-user"></i> No users counted for ' + el.html() + (
-            $('div.register form').length
-            ?
-            ' (Click to be the first ...)<br>'
-            :
-            ''
-          )
-        ) + (
-          country_champion
-          ?
-          '<i class="fa fa-fw fa-trophy"></i> And we have a country champion!<br>'
-          :
-          ''
-        )
-      );
-    },
-    onRegionClick: function(e, code) {
-      if (!$('div.register form').length) return false;
-      $('select[name="workLocation[address][addressCountry]"]').val(code);
-      $('html, body').animate({
-  			scrollTop: $('#user-register').offset().top - 100
-  		}, 500, function() {
-  			if(history.pushState) {
-  				history.pushState(null, null, '#user-register');
-  			} else {
-  				// window.location.hash = link_hash_divided[1];
-  			}
-  		});
-    }
-  });
-  table.hide()
-
-  // --- about ---
-  
-  $('div#about ul>li').addClass("invisible");
-  $('div#about').viewportChecker({
-    offset : 80,
-    callbackFunction : function(){
+    $('form', element).submit(function() {
+      var form = $(this);
+      var loading_indicator = $(this).find('button[type="submit"] .loading-indicator');
+      var action = form.attr('action');
+      var method = form.attr('method');
       
-      $('div#about li').each(function(i){
-        var li = this;
-        setTimeout(function(){
-          $(li).addClass("visible").addClass("animated").addClass("fadeInUp");
-        }, i * 1000);
+      loading_indicator.show();
+      
+      $.ajax({
+        type: method,
+        url: action,
+        data: form.serialize()
+      }).done(function(data) {
+        form.replaceWith( hijax.hijax(hijax.extractBody(data)) );
+        loading_indicator.hide();
+      }).fail(function(jqXHR) {
+        form.replaceWith( hijax.hijax(hijax.extractBody(jqXHR.responseText)) );
+        loading_indicator.hide();
       });
       
-    }
-  });
-
-  // --- hijax behavior ---
+      return false;
+    });
   
-  hijax($('body'));
+    return element;
+  
+  },
+  
+  extractBody : function(html) {
+    return $(html.match(/<\s*body.*>[\s\S]*<\s*\/body\s*>/ig).join(""));
+  }
+}
 
-});
+
+// --- map ---
+
+map = {
+  
+  init : function(){
+    
+    var table = $('table[about="#users-by-country"]');
+    var map = $('#worldmap');
+    var json = JSON.parse(table.find('script').html());
+    var heat_data = {};
+    
+    // hide table	
+    table.hide()
+    
+    // convert heat map data
+    for (i in json.entries) {
+      heat_data[ json.entries[i].key.toUpperCase() ] = json.entries[i].value;
+    }
+    
+    // example heat map data
+    if(false) {
+      heat_data = {
+        "DE" : 15, "CH" : 4, "AT" : 6, "GB" : 12, "FR" : 9, "ES" : 5, "US" : 9, "PL" : 2, "BF" : 1, "NO" : 5, "CN" : 6, "ID" : 4, "GH" : 4, "IR" : 5, "BR" : 7, "CD" : 5, "KZ" : 9, "RU" : 2, "RO" : 4, "DZ" : 3, "CA" : 2
+      };
+    }
+    
+    // init vector map
+    map.vectorMap({
+      backgroundColor: '#0c75bf',
+      zoomButtons: false,
+      zoomOnScroll: false,
+      series: {
+        regions: [{
+          values: heat_data,
+          scale: ['#cfdfba', '#a1cd3f'],
+          normalizeFunction: 'linear'
+        }]
+      },
+      onRegionTipShow: function(e, el, code){
+        var country_champion = false;
+        var users_registered = false;
+  
+        if(
+          $('ul[about="#country-champions"] li[data-country-code="' + code + '"]').length
+        ) {
+          country_champion = true;
+        }
+  
+        if(
+          typeof heat_data[code] != 'undefined'
+        ) {
+          users_registered = true;
+        }
+  
+        el.html(
+          (
+            users_registered
+            ?
+            '<i class="fa fa-fw fa-user"></i> <strong>' + heat_data[code] + '</strong> users counted for ' + el.html() + (
+              $('div.register form').length
+              ?
+              ' (Click to be the next ...)<br>'
+              :
+              ''
+            )
+            :
+            '<i class="fa fa-fw fa-user"></i> No users counted for ' + el.html() + (
+              $('div.register form').length
+              ?
+              ' (Click to be the first ...)<br>'
+              :
+              ''
+            )
+          ) + (
+            country_champion
+            ?
+            '<i class="fa fa-fw fa-trophy"></i> And we have a country champion!<br>'
+            :
+            ''
+          )
+        );
+      },
+      onRegionClick: function(e, code) {
+        if (!$('div.register form').length) return false;
+        $('select[name="workLocation[address][addressCountry]"]').val(code);
+        $('html, body').animate({
+          scrollTop: $('#user-register').offset().top - 100
+        }, 500, function() {
+          if(history.pushState) {
+            history.pushState(null, null, '#user-register');
+          } else {
+            // window.location.hash = link_hash_divided[1];
+          }
+        });
+      }
+    });
+  }
+  
+};
 
 
-// --- blog ---
+// --- other ---
+
+other = {
+  
+  init : function() {
+    
+    // placeholder polyfill		
+    $('input, textarea').placeholder();
+  
+  
+    // animate about section
+    $('div#about ul>li').addClass("invisible");
+    $('div#about').viewportChecker({
+      offset : 80,
+      callbackFunction : function(){
+        $('div#about li').each(function(i){
+          var li = this;
+          setTimeout(function(){
+            $(li).addClass("visible").addClass("animated").addClass("fadeInUp");
+          }, i * 1000);
+        });
+      }
+    });
+    
+  }
+  
+}
+
+
+// --- news ---
 
 google.load("feeds", "1");
 
-function initialize() {
+// needs to be initialized out of $(document).ready(...)
+google.setOnLoadCallback(function() {
   var feed = new google.feeds.Feed("https://oerworldmap.wordpress.com/feed/");
   feed.load(function(result) {
     if (!result.error) {
@@ -230,6 +232,16 @@ function initialize() {
         $('#blog-link').prepend(rendered);
       });
     }
-  });
-}
-google.setOnLoadCallback(initialize);
+  });		
+});
+
+
+// --- document ready ---
+
+$(document).ready(function(){
+  
+  hijax.init();
+  map.init();
+  other.init();
+  
+});
