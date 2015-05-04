904f687592eb3ed8628a014454744e924db61b2c
Philipp v. Böselager <boeselager@hbz-nrw.de>
2015-05-04 16:42:36 +0200

    Introduce amending information to existing resources via request

--- app/controllers/ResourceIndex.java
+++ app/controllers/ResourceIndex.java
@@ -1,27 +1,32 @@
 package controllers;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.github.fge.jsonschema.core.report.ProcessingReport;
 import helpers.Countries;
 import helpers.JSONForm;
-import models.Resource;
-import org.json.simple.parser.ParseException;
-import play.mvc.Result;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+
+import models.Resource;
+
+import org.json.simple.parser.ParseException;
+
+import play.mvc.Result;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.github.fge.jsonschema.core.report.ProcessingReport;
 
 /**
  * @author fo
  */
 public class ResourceIndex extends OERWorldMap {
 
-  public static Result list()  throws IOException, ParseException {
-    List<Resource> stories = mBaseRepository.query("Article", false);
-    Map<String,Object> scope = new HashMap<>();
+  public static Result list() throws IOException, ParseException {
+    List<Resource> stories = mBaseRepository.query("Action", false);
+    Map<String, Object> scope = new HashMap<>();
     scope.put("stories", stories);
 
     if (request().accepts("text/html")) {
@@ -41,7 +46,7 @@ public class ResourceIndex extends OERWorldMap {
     Resource resource = Resource.fromJson(json);
     ProcessingReport report = resource.validate();
     if (!report.isSuccess()) {
-      Map<String,Object> scope = new HashMap<>();
+      Map<String, Object> scope = new HashMap<>();
       scope.put("resource", resource);
       scope.put("countries", Countries.list(currentLocale));
       if (isJsonRequest) {
@@ -63,6 +68,30 @@ public class ResourceIndex extends OERWorldMap {
       return notFound("Not found");
     }
     return ok(render("Home", "ResourceIndex/read.mustache", resource));
+  }
+
+  /**
+   * This method is designed to add information to existing resources. If the
+   * resource doesn't exist yet, a bad request response is returned
+   * 
+   * @param id
+   * @param json
+   * @return
+   * @throws IOException
+   */
+  public static Result put(String id, String json) throws IOException {
+    Resource resource = mBaseRepository.getResource(id);
+    if (resource == null) {
+      return badRequest("missing resource " + id);
+    }
+    JsonNode jsonNode = JsonNodeFactory.instance.textNode(json);
+    Iterator<Map.Entry<String, JsonNode>> it = jsonNode.fields();
+    while (it.hasNext()) {
+      Map.Entry<String, JsonNode> entry = (Map.Entry<String, JsonNode>) it.next();
+      resource.put(entry.getKey(), entry.getValue());
+    }
+    mBaseRepository.addResource(resource);
+    return created("created resource " + resource.toString());
   }
 
 }
