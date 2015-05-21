da020a65ae3bf2db70a4f6a7b5776d20086a58af
Felix Ostrowski <felix.ostrowski@googlemail.com>
2015-05-21 13:49:54 +0200

    Decouple column size from role

--- public/javascripts/hijax.js
+++ public/javascripts/hijax.js
@@ -8,7 +8,7 @@ Hijax = {
       for (var i=0;i<vars.length;i++) {
         var pair = vars[i].split("=");
         if (pair[0] == variable) {
-          return pair[1];
+          return decodeURIComponent(pair[1]);
         }
       }
       return(false);
