1c47a67af16e1cf6a10e15c17e5d873e17631c89
joh <j@sights.de>
2015-06-17 17:38:28 +0200

    Setup #map size sensitive  zoom
    - Hijax.behaviours.map.getZoomValues that returns, initial-, min- and max-zoom relative to the size of the map
    - Setup and adjust standart zoom values
    - Adjust map center
    - Cleanup Hijax.behaviours.map.attach()
    - Remove projections plugin "proj4js"

--- public/javascripts/behaviours/map.js
+++ public/javascripts/behaviours/map.js
@@ -5,27 +5,19 @@ Hijax.behaviours.map = {
   vector : null,
   vectorSource : null,
   projection: null,
+  
+  standartMapSize: [896, 655],
+  standartInitialZoom: 1.85,
+  standartMinZoom: 1.85,
+  standartMaxZoom: 8,
 
   attach : function(context) {
     var map = this;
 
     $('div[data-view="map"]', context).each(function() {
-
-      proj4.defs('ESRI:53009', '+proj=moll +lon_0=0 +x_0=0 +y_0=0 +a=6371000 ' +
-          '+b=6371000 +units=m +no_defs');
-
-      // Configure the Sphere Mollweide projection object with an extent,
-      // and a world extent. These are required for the Graticule.
-      var sphereMollweideProjection = new ol.proj.Projection({
-        code: 'ESRI:53009',
-        extent: [-9009954.605703328, -9009954.605703328,
-          9009954.605703328, 9009954.605703328],
-        worldExtent: [-179, -90, 179, 90]
-      });
-
-      //map.projection = sphereMollweideProjection;
+      
+      // Get mercator projection
       map.projection = ol.proj.get('EPSG:3857');
-      //map.projection = ol.proj.get('WGS84');
 
       // Map container
       map.container = $('<div id="map"><div id="popup"></div></div>')[0];
@@ -43,14 +35,19 @@ Hijax.behaviours.map = {
       map.vector = new ol.layer.Vector({
         source: map.vectorSource
       });
-
+      
+      // Get zoom values adapted to map size
+      var zoom_values = map.getZoomValues();
+      
       // View
       map.view = new ol.View({
-        center: [0, 0],
+        center: [0, 5000000],
         projection: map.projection,
-        zoom: 1
+        zoom: zoom_values.initialZoom,
+        minZoom: zoom_values.minZoom,
+        maxZoom: zoom_values.maxZoom
       });
-
+      
       // Map object
       map.world = new ol.Map({
         layers: [map.vector],
@@ -59,6 +56,7 @@ Hijax.behaviours.map = {
       });
 
       // User position
+/*
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
           var lon = position.coords.latitude;
@@ -66,16 +64,7 @@ Hijax.behaviours.map = {
           map.world.getView().setCenter(center);
         });
       }
-
-      // Grid
-      /*var graticule = new ol.Graticule({
-        map: map.world,
-        strokeStyle: new ol.style.Stroke({
-          color: 'rgba(255,120,0,0.9)',
-          width: 1.5,
-          lineDash: [0.5, 4]
-        })
-      });*/
+*/
 
       map.world.on('pointermove', function(evt) {
         if (evt.dragging) {
@@ -87,7 +76,7 @@ Hijax.behaviours.map = {
         map.displayFeatureInfo(pixel, context);
       });
 
-      map.world.on('click', function(evt) {
+      map.world.on('click', function(evt) {        
         var feature = map.world.forEachFeatureAtPixel(evt.pixel, function(feature, layer) {
           return feature;
         });
@@ -100,28 +89,52 @@ Hijax.behaviours.map = {
           }
         }
       });
-
+      
+      // zoom to bounding box, if focus is set
       map.setBoundingBox(this);
-      $(this).addClass("map-view");
       
-      map.popupElement = document.getElementById('popup');
-      
+      // init popovers
+      map.popupElement = document.getElementById('popup');      
       map.popup = new ol.Overlay({
         element: map.popupElement,
         positioning: 'bottom-center',
         stopEvent: false
       });
-      
       map.world.addOverlay(map.popup);
-      
       map.templates = {};
-      
       $.get('/assets/mustache/popover.mustache', function(data){
         map.templates.popover = data;
       });
+      
+      // switch style
+      $(this).addClass("map-view");
 
     });
 
+  },
+  
+  getZoomValues : function() {
+    
+    if(
+      $('#map').width() / $('#map').height()
+      >
+      this.standartMapSize[0] / this.standartMapSize[1]
+    ) {
+      // current aspect ratio is more landscape then standart, so take height as constraint
+      var size_vactor = $('#map').height() / this.standartMapSize[1];
+    } else {
+      // it's more portrait, so take width
+      var size_vactor = $('#map').width() / this.standartMapSize[0];
+    }
+    
+    size_vactor = Math.pow(size_vactor, 0.5);
+    
+    return {
+      initialZoom: this.standartInitialZoom * size_vactor,
+      minZoom: this.standartMinZoom * size_vactor,
+      maxZoom: this.standartMaxZoom * size_vactor
+    };
+    
   },
 
   displayedFeatureInfo : null,
@@ -408,4 +421,91 @@ Hijax.behaviours.map = {
 
   }
 
-}
+};
+
+
+// Mollweide Projection
+
+/*
+      proj4.defs('ESRI:53009', '+proj=moll +lon_0=0 +x_0=0 +y_0=0 +a=6371000 ' +
+          '+b=6371000 +units=m +no_defs');
+*/
+
+      // Configure the Sphere Mollweide projection object with an extent,
+      // and a world extent. These are required for the Graticule.
+/*
+      var sphereMollweideProjection = new ol.proj.Projection({
+        code: 'ESRI:53009',
+        extent: [-9009954.605703328, -9009954.605703328,
+          9009954.605703328, 9009954.605703328],
+        worldExtent: [-179, -90, 179, 90]
+      });
+*/
+
+/*
+      map.projection.setWorldExtent(
+        [-180, -10, 180, 10]
+      );
+*/
+      
+/*
+      console.log("extent", map.projection.getExtent());
+      console.log("worldExtent", map.projection.getWorldExtent());
+      console.log("global", map.projection.isGlobal());
+*/
+      
+/*
+      map.projection.setExtent(
+        [-20037508.342789244, -1037508.342789244, 20037508.342789244, 1037508.342789244]
+      );
+*/
+
+
+/*
+      var extent = [-20037508.342789244, -1037508.342789244, 20037508.342789244, 1037508.342789244];
+      var constrainPan = function() {
+          var visible = map.view.calculateExtent(map.world.getSize());
+          var centre = map.view.getCenter();
+          var delta;
+          var adjust = false;
+          if ((delta = extent[0] - visible[0]) > 0) {
+              adjust = true;
+              centre[0] += delta;
+          } else if ((delta = extent[2] - visible[2]) < 0) {
+              adjust = true;
+              centre[0] += delta;
+          }
+          if ((delta = extent[1] - visible[1]) > 0) {
+              adjust = true;
+              centre[1] += delta;
+          } else if ((delta = extent[3] - visible[3]) < 0) {
+              adjust = true;
+              centre[1] += delta;
+          }
+          if (adjust) {
+              map.view.setCenter(centre);
+          }
+      };
+      map.view.on('change:resolution', constrainPan);
+      map.view.on('change:center', constrainPan);
+*/
+
+      
+      // Zoom to extent ... extent seem to be buggy, so this is done by initial zoom relative to mapsize
+      // extent format: [minX, minY, maxX, maxY]
+/*
+      var mapSize = map.world.getSize();
+      var extent = map.vector.getSource().getExtent();
+      extent = [-13037508.342789244, -5000000, 13037508.342789244, 13000000]
+      map.world.getView().fitExtent(extent, mapSize);
+*/
+
+      // Grid
+      /*var graticule = new ol.Graticule({
+        map: map.world,
+        strokeStyle: new ol.style.Stroke({
+          color: 'rgba(255,120,0,0.9)',
+          width: 1.5,
+          lineDash: [0.5, 4]
+        })
+      });*/
