9ee419ceb510271e29c27518c87fc9a63ca2cd7e
Felix Ostrowski <felix.ostrowski@googlemail.com>
2015-06-18 14:17:15 +0200

    Add aggregation to single country view

--- app/controllers/CountryIndex.java
+++ app/controllers/CountryIndex.java
@@ -1,9 +1,13 @@
 package controllers;
 
 import helpers.Countries;
+import models.Record;
 import models.Resource;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
 import play.mvc.Result;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -14,10 +18,19 @@ import java.util.Map;
  */
 public class CountryIndex extends OERWorldMap {
 
-  public static Result read(String id) {
+  public static Result read(String id) throws IOException {
     if (! Arrays.asList(java.util.Locale.getISOCountries()).contains(id.toUpperCase())) {
       return notFound("Not found");
     }
+
+    AggregationBuilder usersByCountry = AggregationBuilders.terms("users_by_country").field(
+        Record.RESOURCEKEY + ".workLocation.address.addressCountry").include(id).size(0);
+    AggregationBuilder organizationsByCountry = AggregationBuilders.terms("organizations_by_country").field(
+        Record.RESOURCEKEY + ".location.address.addressCountry").include(id).size(0);
+    AggregationBuilder championsByCountry = AggregationBuilders.terms("champions_by_country").field(
+        Record.RESOURCEKEY + ".countryChampionFor").include(id).size(0);
+    Resource countryAggregation = mBaseRepository.query(Arrays.asList(usersByCountry, organizationsByCountry, championsByCountry));
+
 
     List<Resource> champions = mBaseRepository.esQuery("countryChampionFor:".concat(id.toUpperCase()));
     List<Resource> resources = mBaseRepository.esQuery("about.\\*.addressCountry:".concat(id.toUpperCase()));
@@ -27,6 +40,7 @@ public class CountryIndex extends OERWorldMap {
     scope.put("name", Countries.getNameFor(id, currentLocale));
     scope.put("champions", champions);
     scope.put("resources", resources);
+    scope.put("countryAggregation", countryAggregation);
 
     if (request().accepts("text/html")) {
       return ok(render(Countries.getNameFor(id, currentLocale), "CountryIndex/read.mustache", scope));
