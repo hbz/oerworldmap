f91f941d7c8d20ef1f5d09bc7f99f3ffe8ec4174
Felix Ostrowski <felix.ostrowski@googlemail.com>
2015-07-22 15:34:21 +0200

    Load static pages from JAR
    Move templates to public/pages/
    
    Fixes #287

--- app/controllers/StaticPage.java
+++ app/controllers/StaticPage.java
@@ -1,12 +1,15 @@
 package controllers;
 
 import helpers.UniversalFunctions;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.pegdown.PegDownProcessor;
 import play.Logger;
+import play.Play;
 import play.mvc.Result;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
@@ -25,20 +28,23 @@ public class StaticPage extends OERWorldMap {
     String language = currentLocale.getLanguage();
     String country = currentLocale.getCountry();
     String extension = ".md";
-    String path = play.Play.application().path().getAbsolutePath() + "/data/pages/";
+    String path = "public/pages/";
+    ClassLoader classLoader = Play.application().classloader();
+    String titleLocalePath = path.concat(title).concat("_").concat(language).concat("_").concat(country)
+        .concat(extension);
+    String titleLanguagePath = path.concat(title).concat("_").concat(language).concat(extension);
+    String titlePath = path.concat(title).concat(extension);
     String body;
 
     try {
-      body = UniversalFunctions.readFile(path.concat(title.concat("_").concat(language).concat("_").concat(country)
-          .concat(extension)), StandardCharsets.UTF_8);
-    } catch (IOException noLocale) {
+      body = IOUtils.toString(classLoader.getResourceAsStream(titleLocalePath));
+    } catch (NullPointerException | IOException noLocale) {
       try {
-        body = UniversalFunctions.readFile(path.concat(title.concat("_").concat(language).concat(extension)),
-            StandardCharsets.UTF_8);
-      } catch (IOException noLang) {
+        body = IOUtils.toString(classLoader.getResourceAsStream(titleLanguagePath));
+      } catch (NullPointerException | IOException noLanguage) {
         try {
-          body = UniversalFunctions.readFile(path.concat(title.concat(extension)), StandardCharsets.UTF_8);
-        } catch (IOException noPage) {
+          body = IOUtils.toString(classLoader.getResourceAsStream(titlePath));
+        } catch (NullPointerException | IOException noPage) {
           return notFound("Page not found");
         }
       }
