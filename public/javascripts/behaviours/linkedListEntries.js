0c309d68e691700536864572075774cbe7cf3dca
Felix Ostrowski <felix.ostrowski@googlemail.com>
2015-08-19 16:45:36 +0200

    Fix SyntaxError when parsing embedded JSON

--- public/javascripts/behaviours/linkedListEntries.js
+++ public/javascripts/behaviours/linkedListEntries.js
@@ -12,24 +12,30 @@ Hijax.behaviours.linkedListEntries = {
     $('[data-behaviour="linkedListEntries"]', context).each(function(){
       $( this ).on("mouseenter", "li", function() {
         var id = this.getAttribute("about");
-        var json = JSON.parse( $(this).closest("ul").children('script[type="application/ld+json"]').html() );
-        var resource = json.filter(function(resource) {
-          return resource['@id'] == id;
-        })[0];
-        var markers = Hijax.behaviours.map.getMarkers(resource);
-        for (var i = 0; i < markers.length; i++) {
-          markers[i].setStyle(Hijax.behaviours.map.styles.placemark.hover());
+        var script = $(this).closest("ul").children('script[type="application/ld+json"]');
+        if (script.length) {
+          var json = JSON.parse( script.html() );
+          var resource = json.filter(function(resource) {
+            return resource['@id'] == id;
+          })[0];
+          var markers = Hijax.behaviours.map.getMarkers(resource);
+          for (var i = 0; i < markers.length; i++) {
+            markers[i].setStyle(Hijax.behaviours.map.styles.placemark.hover());
+          }
         }
       });
       $( this ).on("mouseleave", "li", function() {
         var id = this.getAttribute("about");
-        var json = JSON.parse( $(this).closest("ul").children('script[type="application/ld+json"]').html() );
-        var resource = json.filter(function(resource) {
-          return resource['@id'] == id;
-        })[0];
-        var markers = Hijax.behaviours.map.getMarkers(resource);
-        for (var i = 0; i < markers.length; i++) {
-          markers[i].setStyle(Hijax.behaviours.map.styles.placemark.base());
+        var script = $(this).closest("ul").children('script[type="application/ld+json"]');
+        if (script.length) {
+          var json = JSON.parse( script.html() );
+          var resource = json.filter(function(resource) {
+            return resource['@id'] == id;
+          })[0];
+          var markers = Hijax.behaviours.map.getMarkers(resource);
+          for (var i = 0; i < markers.length; i++) {
+            markers[i].setStyle(Hijax.behaviours.map.styles.placemark.base());
+          }
         }
       });
     });
