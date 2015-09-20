840ac134de1d52c19d716d05f1876937f88720c4
Felix Ostrowski <felix.ostrowski@googlemail.com>
2015-09-20 18:40:51 +0200

    Generalize & simplify aggregations

--- app/services/AggregationProvider.java
+++ app/services/AggregationProvider.java
@@ -16,28 +16,6 @@ public class AggregationProvider {
   private static String resource_field = Record.RESOURCEKEY + ".location.address.addressCountry";
   private static String mentions_field = Record.RESOURCEKEY + ".mentions.location.address.addressCountry";
   private static String provider_field = Record.RESOURCEKEY + ".provider.location.address.addressCountry";
-  private static String participant_field = Record.RESOURCEKEY + ".participant.location.address.addressCountry";
-  private static String agent_field = Record.RESOURCEKEY + ".agent.location.address.addressCountry";
-
-  public static AggregationBuilder<?> getGlobalAggregation() {
-    return AggregationBuilders
-        .terms("global").field("@type")
-        .subAggregation(AggregationBuilders
-            .filter("organizations")
-            .filter(FilterBuilders.termFilter(Record.RESOURCEKEY + ".@type", "Organization")))
-        .subAggregation(AggregationBuilders
-            .filter("users")
-            .filter(FilterBuilders.termFilter(Record.RESOURCEKEY + ".@type", "Person")))
-        .subAggregation(AggregationBuilders
-            .filter("articles")
-            .filter(FilterBuilders.termFilter(Record.RESOURCEKEY + ".@type", "Article")))
-        .subAggregation(AggregationBuilders
-            .filter("services")
-            .filter(FilterBuilders.termFilter(Record.RESOURCEKEY + ".@type", "Service")))
-        .subAggregation(AggregationBuilders
-            .filter("projects")
-            .filter(FilterBuilders.termFilter(Record.RESOURCEKEY + ".@type", "Action")));
-  }
 
   public static AggregationBuilder<?> getTypeAggregation() {
     return AggregationBuilders.terms("about.@type").field("about.@type");
@@ -62,24 +40,8 @@ public class AggregationProvider {
     return AggregationBuilders
         .terms("by_country").script("doc['"
             + resource_field + "'].values + doc['" + mentions_field + "'].values  + doc['"
-            + provider_field + "'].values  + doc['" + participant_field + "'].values  + doc['"
-            + agent_field + "'].values").size(0)
-        .subAggregation(AggregationBuilders
-            .filter("organizations")
-            .filter(FilterBuilders.termFilter(Record.RESOURCEKEY + ".@type", "Organization")))
-        .subAggregation(AggregationBuilders
-            .filter("users")
-            .filter(FilterBuilders.termFilter(Record.RESOURCEKEY + ".@type", "Person")))
-        .subAggregation(AggregationBuilders
-            .filter("articles")
-            .filter(FilterBuilders.termFilter(Record.RESOURCEKEY + ".@type", "Article")))
-        .subAggregation(AggregationBuilders
-            .filter("services")
-            .filter(FilterBuilders.termFilter(Record.RESOURCEKEY + ".@type", "Service")))
-        .subAggregation(AggregationBuilders
-            .filter("projects")
-            .filter(FilterBuilders.termFilter(Record.RESOURCEKEY + ".@type", "Action")))
-            // TODO: The following implies that somebody can be a champion for another country. Is this correct?
+            + provider_field + "'].values").size(0)
+        .subAggregation(AggregationBuilders.terms("by_type").field("about.@type"))
         .subAggregation(AggregationBuilders
             .filter("champions")
             .filter(FilterBuilders.existsFilter(Record.RESOURCEKEY + ".countryChampionFor")));
@@ -89,27 +51,11 @@ public class AggregationProvider {
     return AggregationBuilders
         .terms("by_country").script("doc['"
             + resource_field + "'].values + doc['" + mentions_field + "'].values  + doc['"
-            + provider_field + "'].values  + doc['" + participant_field + "'].values + doc['"
-            + agent_field + "'].values").include(id).size(0)
-        .subAggregation(AggregationBuilders
-            .filter("organizations")
-            .filter(FilterBuilders.termFilter(Record.RESOURCEKEY + ".@type", "Organization")))
-        .subAggregation(AggregationBuilders
-            .filter("users")
-            .filter(FilterBuilders.termFilter(Record.RESOURCEKEY + ".@type", "Person")))
-        .subAggregation(AggregationBuilders
-            .filter("articles")
-            .filter(FilterBuilders.termFilter(Record.RESOURCEKEY + ".@type", "Article")))
-        .subAggregation(AggregationBuilders
-            .filter("services")
-            .filter(FilterBuilders.termFilter(Record.RESOURCEKEY + ".@type", "Service")))
-        .subAggregation(AggregationBuilders
-            .filter("projects")
-            .filter(FilterBuilders.termFilter(Record.RESOURCEKEY + ".@type", "Action")))
-            // TODO: The following implies that somebody can only be a chamption for her country. Is this correct?
+            + provider_field + "'].values").include(id).size(0)
+        .subAggregation(AggregationBuilders.terms("by_type").field("about.@type"))
         .subAggregation(AggregationBuilders
             .filter("champions")
-            .filter(FilterBuilders.termFilter(Record.RESOURCEKEY + ".countryChampionFor", id)));
+            .filter(FilterBuilders.existsFilter(Record.RESOURCEKEY + ".countryChampionFor")));
   }
 
 }
