ed2d6f13b308c343edeba57e87534afb7b27d438
Felix Ostrowski <felix.ostrowski@googlemail.com>
2015-09-24 13:11:02 +0200

    Add URI template support for links from pie charts

--- public/javascripts/helpers/shared.js
+++ public/javascripts/helpers/shared.js
@@ -2,7 +2,190 @@ var console = console || {
   log: function(message) {
     java.lang.System.out.println(JSON.stringify(message, null, 2));
   }
-}
+};
+
+/**
+ * https://github.com/bramstein/url-template
+ */
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else {
+        root.urltemplate = factory();
+    }
+}(this, function () {
+  /**
+   * @constructor
+   */
+  function UrlTemplate() {
+  }
+
+  /**
+   * @private
+   * @param {string} str
+   * @return {string}
+   */
+  UrlTemplate.prototype.encodeReserved = function (str) {
+    return str.split(/(%[0-9A-Fa-f]{2})/g).map(function (part) {
+      if (!/%[0-9A-Fa-f]/.test(part)) {
+        part = encodeURI(part);
+      }
+      return part;
+    }).join('');
+  };
+
+  /**
+   * @private
+   * @param {string} operator
+   * @param {string} value
+   * @param {string} key
+   * @return {string}
+   */
+  UrlTemplate.prototype.encodeValue = function (operator, value, key) {
+    value = (operator === '+' || operator === '#') ? this.encodeReserved(value) : encodeURIComponent(value);
+
+    if (key) {
+      return encodeURIComponent(key) + '=' + value;
+    } else {
+      return value;
+    }
+  };
+
+  /**
+   * @private
+   * @param {*} value
+   * @return {boolean}
+   */
+  UrlTemplate.prototype.isDefined = function (value) {
+    return value !== undefined && value !== null;
+  };
+
+  /**
+   * @private
+   * @param {string}
+   * @return {boolean}
+   */
+  UrlTemplate.prototype.isKeyOperator = function (operator) {
+    return operator === ';' || operator === '&' || operator === '?';
+  };
+
+  /**
+   * @private
+   * @param {Object} context
+   * @param {string} operator
+   * @param {string} key
+   * @param {string} modifier
+   */
+  UrlTemplate.prototype.getValues = function (context, operator, key, modifier) {
+    var value = context[key],
+        result = [];
+
+    if (this.isDefined(value) && value !== '') {
+      if (typeof value === 'string' || typeof value === 'number' || typeof value === 'boolean') {
+        value = value.toString();
+
+        if (modifier && modifier !== '*') {
+          value = value.substring(0, parseInt(modifier, 10));
+        }
+
+        result.push(this.encodeValue(operator, value, this.isKeyOperator(operator) ? key : null));
+      } else {
+        if (modifier === '*') {
+          if (Array.isArray(value)) {
+            value.filter(this.isDefined).forEach(function (value) {
+              result.push(this.encodeValue(operator, value, this.isKeyOperator(operator) ? key : null));
+            }, this);
+          } else {
+            Object.keys(value).forEach(function (k) {
+              if (this.isDefined(value[k])) {
+                result.push(this.encodeValue(operator, value[k], k));
+              }
+            }, this);
+          }
+        } else {
+          var tmp = [];
+
+          if (Array.isArray(value)) {
+            value.filter(this.isDefined).forEach(function (value) {
+              tmp.push(this.encodeValue(operator, value));
+            }, this);
+          } else {
+            Object.keys(value).forEach(function (k) {
+              if (this.isDefined(value[k])) {
+                tmp.push(encodeURIComponent(k));
+                tmp.push(this.encodeValue(operator, value[k].toString()));
+              }
+            }, this);
+          }
+
+          if (this.isKeyOperator(operator)) {
+            result.push(encodeURIComponent(key) + '=' + tmp.join(','));
+          } else if (tmp.length !== 0) {
+            result.push(tmp.join(','));
+          }
+        }
+      }
+    } else {
+      if (operator === ';') {
+        result.push(encodeURIComponent(key));
+      } else if (value === '' && (operator === '&' || operator === '?')) {
+        result.push(encodeURIComponent(key) + '=');
+      } else if (value === '') {
+        result.push('');
+      }
+    }
+    return result;
+  };
+
+  /**
+   * @param {string} template
+   * @return {function(Object):string}
+   */
+  UrlTemplate.prototype.parse = function (template) {
+    var that = this;
+    var operators = ['+', '#', '.', '/', ';', '?', '&'];
+
+    return {
+      expand: function (context) {
+        return template.replace(/\{([^\{\}]+)\}|([^\{\}]+)/g, function (_, expression, literal) {
+          if (expression) {
+            var operator = null,
+                values = [];
+
+            if (operators.indexOf(expression.charAt(0)) !== -1) {
+              operator = expression.charAt(0);
+              expression = expression.substr(1);
+            }
+
+            expression.split(/,/g).forEach(function (variable) {
+              var tmp = /([^:\*]*)(?::(\d+)|(\*))?/.exec(variable);
+              values.push.apply(values, that.getValues(context, operator, tmp[1], tmp[2] || tmp[3]));
+            });
+
+            if (operator && operator !== '+') {
+              var separator = ',';
+
+              if (operator === '?') {
+                separator = '&';
+              } else if (operator !== '#') {
+                separator = operator;
+              }
+              return (values.length !== 0 ? operator : '') + values.join(separator);
+            } else {
+              return values.join(',');
+            }
+          } else {
+            return that.encodeReserved(literal);
+          }
+        });
+      }
+    };
+  };
+
+  return new UrlTemplate();
+}));
 
 Handlebars.registerHelper('getField', function (string, options) {
   var parts = string.split('.');
@@ -91,12 +274,12 @@ Handlebars.registerHelper('getBundle', function (field, options) {
     var d = "M" + (width/2) + "," + (height/2)+ "  L" + x1 + "," + y1 + "  A" + ((width/2)*.975) + "," + ((height/2)*.975) + " 0 " +
         ((endAngle-startAngle > 180) ? 1 : 0) + ",1 " + x2 + "," + y2 + " z";
 
-    var c = parseInt(i / sectorAngleArr.length * 360);
-    var path = createElement("path", true, {d: d, fill: "hsl(" + c + ", 66%, 50%)"});//,
-        //"<title>" + buckets[i]['key'] + " (" + buckets[i]['doc_count'] + ")</title>"
-    //);
+    var c = parseInt((i + 200) / sectorAngleArr.length * 360);
+    var path = createElement("path", true, {d: d, fill: "hsl(" + c + ", 66%, 50%)"});
+
+    var href = urltemplate.parse(options.hash['href-template']).expand(buckets[i]);
     var arc = createElement("a", true, {
-      "xlink:href": "/resource/?filter.about.@type=" + buckets[i]['key'],
+      "xlink:href": href,
       "xlink:title": buckets[i]['key'] + " (" + buckets[i]['doc_count'] + ")"
     }, path);
     arcs.push(arc);
