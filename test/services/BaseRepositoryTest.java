ecceef6ab90f836fe39d95b5e78c69f4a6e81303
Philipp v. Böselager <boeselager@hbz-nrw.de>
2015-09-24 13:56:07 +0200

    task/#383: Eliminate Java warnings

--- test/services/BaseRepositoryTest.java
+++ test/services/BaseRepositoryTest.java
@@ -1,17 +1,7 @@
 package services;
 
-import controllers.Global;
-import helpers.ElasticsearchHelpers;
-import helpers.JsonTest;
-
 import java.io.File;
 import java.io.IOException;
-
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-import helpers.ElasticsearchHelpers;
-import helpers.UniversalFunctions;
-import models.Resource;
 
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.ImmutableSettings;
@@ -22,9 +12,15 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+import helpers.ElasticsearchHelpers;
+import helpers.JsonTest;
+import models.Resource;
 import services.repository.BaseRepository;
 
-public class BaseRepositoryTest implements JsonTest{
+public class BaseRepositoryTest implements JsonTest {
 
   private static BaseRepository mRepo;
   private static Settings mClientSettings;
@@ -41,8 +37,8 @@ public class BaseRepositoryTest implements JsonTest{
     mClientSettings = ImmutableSettings.settingsBuilder().put(mEsConfig.getClientSettings())
         .build();
     mClient = new TransportClient(mClientSettings)
-        .addTransportAddress(new InetSocketTransportAddress(mEsConfig.getServer(), Integer
-            .valueOf(mEsConfig.getJavaPort())));
+        .addTransportAddress(new InetSocketTransportAddress(mEsConfig.getServer(),
+            Integer.valueOf(mEsConfig.getJavaPort())));
     mElClient = new ElasticsearchProvider(mClient, mEsConfig);
     mElClient.createIndex(mConfig.getString("es.index.name"));
     mRepo = new BaseRepository(mConfig);
@@ -54,8 +50,10 @@ public class BaseRepositoryTest implements JsonTest{
     String property = "attended";
     Resource value = new Resource("Event", "OER15");
     resource.put(property, value);
-    Resource expected1 = getResourceFromJsonFile("BaseRepositoryTest/testResourceWithIdentifiedSubObject.OUT.1.json");
-    Resource expected2 = getResourceFromJsonFile("BaseRepositoryTest/testResourceWithIdentifiedSubObject.OUT.2.json");
+    Resource expected1 = getResourceFromJsonFile(
+        "BaseRepositoryTest/testResourceWithIdentifiedSubObject.OUT.1.json");
+    Resource expected2 = getResourceFromJsonFile(
+        "BaseRepositoryTest/testResourceWithIdentifiedSubObject.OUT.2.json");
     mRepo.addResource(resource);
     Assert.assertEquals(expected1, mRepo.getResource("id001"));
     Assert.assertEquals(expected2, mRepo.getResource("OER15"));
@@ -66,11 +64,12 @@ public class BaseRepositoryTest implements JsonTest{
     Resource resource = new Resource("Person", "id002");
     Resource value = new Resource("Foo", null);
     resource.put("attended", value);
-    Resource expected = getResourceFromJsonFile("BaseRepositoryTest/testResourceWithUnidentifiedSubObject.OUT.1.json");
+    Resource expected = getResourceFromJsonFile(
+        "BaseRepositoryTest/testResourceWithUnidentifiedSubObject.OUT.1.json");
     mRepo.addResource(resource);
     Assert.assertEquals(expected, mRepo.getResource("id002"));
   }
-  
+
   @AfterClass
   public static void clean() throws IOException {
     ElasticsearchHelpers.cleanIndex(mElClient, mEsConfig.getIndex());
