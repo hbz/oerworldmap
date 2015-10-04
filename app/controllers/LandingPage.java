1661576c24fcd38e0b78c81a0cc5471040383481
Felix Ostrowski <felix.ostrowski@googlemail.com>
2015-10-04 15:41:40 +0200

    Add type statistics table to landing page

--- app/controllers/LandingPage.java
+++ app/controllers/LandingPage.java
@@ -4,13 +4,17 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import models.Resource;
 import play.mvc.Result;
+import services.AggregationProvider;
 
 public class LandingPage extends OERWorldMap {
 
   public static Result get() throws IOException {
 
     Map<String, Object> scope = new HashMap<>();
+    Resource typeAggregation = mBaseRepository.aggregate(AggregationProvider.getTypeAggregation());
+    scope.put("typeAggregation", typeAggregation);
     return ok(render("OER World Map", "LandingPage/index.mustache", scope));
 
   }
