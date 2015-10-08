40deb6c19dda068b6ae5f62d8e1a32b16cdf790b
Felix Ostrowski <felix.ostrowski@googlemail.com>
2015-10-08 15:12:09 +0200

    Migrate all alternate names to localized strings
    Fixes #291

--- scripts/migrations/localizedStringsToArray.py
+++ scripts/migrations/localizedStringsToArray.py
@@ -3,15 +3,17 @@ def migrate(resource):
 
 def localized_strings_to_array(resource):
     for property in resource.keys():
-        if property in [ "description", "alternateName"] and not isinstance(resource[property], list):
-            print "Migrating " + property + " to list for " + resource['@id']
-            if isinstance(resource[property], dict):
+        if property in [ "description", "alternateName"]:
+            if not isinstance(resource[property], list):
+                print "Migrating " + property + " to list for " + resource['@id']
                 resource[property] = [resource[property]]
-            else:
-                resource[property] = [{
-                    "@value": resource[property],
-                    "@language": "en"
-                }]
+            for i, val in enumerate(resource[property]):
+                if not isinstance(resource[property][i], dict):
+                    print "Migrating string " + property + " to object for " + resource['@id']
+                    resource[property] = [{
+                        "@value": resource[property][i],
+                        "@language": "en"
+                    }]
         elif isinstance(resource[property], list):
             for i, val in enumerate(resource[property]):
                 if isinstance(val, dict):
