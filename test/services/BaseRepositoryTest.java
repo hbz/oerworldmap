48211b01e84c4bcf187d65e5e4b94209cc39cddb
Felix Ostrowski <felix.ostrowski@googlemail.com>
2015-11-05 16:14:08 +0100

    Add test case

--- test/services/BaseRepositoryTest.java
+++ test/services/BaseRepositoryTest.java
@@ -40,7 +40,7 @@ public class BaseRepositoryTest extends ElasticsearchTestGrid implements JsonTes
   }
 
   @Test
-  public void testDeleteResourceWithMentionedResources() throws IOException, InterruptedException {
+  public void testDeleteResourceWithMentionedResources() throws IOException {
     // setup: 1 Person ("in1") who has 2 affiliations
     Resource in = getResourceFromJsonFile(
         "BaseRepositoryTest/testDeleteResourceWithMentionedResources.IN.1.json");
@@ -65,7 +65,22 @@ public class BaseRepositoryTest extends ElasticsearchTestGrid implements JsonTes
   }
 
   @Test
-  public void testGetResourcesWithWildcard() throws IOException, InterruptedException {
+  public void testDeleteLastResourceInList() throws IOException {
+    Resource db1 = getResourceFromJsonFile(
+      "BaseRepositoryTest/testDeleteLastResourceInList.DB.1.json");
+    Resource db2 = getResourceFromJsonFile(
+      "BaseRepositoryTest/testDeleteLastResourceInList.DB.2.json");
+    Resource out = getResourceFromJsonFile(
+      "BaseRepositoryTest/testDeleteLastResourceInList.OUT.1.json");
+    mBaseRepo.addResource(db1);
+    mBaseRepo.addResource(db2);
+    mBaseRepo.deleteResource("urn:uuid:3a25e950-a3c0-425d-946d-9806665ec665");
+    Assert.assertNull(mBaseRepo.getResource("urn:uuid:3a25e950-a3c0-425d-946d-9806665ec665"));
+    Assert.assertEquals(out, mBaseRepo.getResource("urn:uuid:c7f5334a-3ddb-4e46-8653-4d8c01e25503"));
+  }
+
+  @Test
+  public void testGetResourcesWithWildcard() throws IOException {
     Resource in1 = getResourceFromJsonFile(
         "BaseRepositoryTest/testGetResourcesWithWildcard.DB.1.json");
     Resource in2 = getResourceFromJsonFile(
