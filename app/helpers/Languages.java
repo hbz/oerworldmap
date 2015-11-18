dbb945fdddc06ba7562c86f960c98b4a6115cbd9
Felix Ostrowski <felix.ostrowski@googlemail.com>
2015-11-18 12:43:37 +0100

    Add languages to client-side i18n

--- app/helpers/Languages.java
+++ app/helpers/Languages.java
@@ -1,11 +1,22 @@
 package helpers;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 /**
  * @author fo
  */
 public class Languages {
+
+  public static Map<String,String> map(Locale aLocale) {
+    Map<String,String> languageMap = new HashMap<>();
+    for (String languageCode : Locale.getISOLanguages()) {
+      Locale language = new Locale(languageCode);
+      languageMap.put(language.getISO3Language(), language.getDisplayLanguage(aLocale));
+    }
+    return languageMap;
+  }
 
   public static String propertyString(Locale aLocale) {
     String propertyString = "";
