bf9ecf21198818bdec266d69f3bca99e5cc07f46
joh <j@sights.de>
2015-11-26 17:35:55 +0100

    Interim commit: localizedInput with typeahead.js
    ... not working yet.

--- public/javascripts/behaviours/resource-form-widgets/localizedInput.js
+++ public/javascripts/behaviours/resource-form-widgets/localizedInput.js
@@ -15,7 +15,9 @@ var Hijax = (function ($, Hijax) {
 
           var fieldset = this;
 
-          var value_input = $(this)
+          // enhance layout
+
+          var value_input = $(fieldset)
             .find('[name*="@value"]')
             .addClass('form-control')
             .detach()[0].outerHTML;
@@ -27,13 +29,70 @@ var Hijax = (function ($, Hijax) {
             })
           );
 
+          // save controls
+
+          var language_button = $(fieldset).find('button');
+          var dropdown = $(fieldset).find('.dropdown-menu')
+          var dropdown_parent = dropdown.parent();
+          var language_input = $(fieldset).find('[name*="@language"]');
+
+          // init typeahead
+
+          $(fieldset).find('.dropdown-menu').on('click', function(){
+            return false; // dirty
+          });
+
+          var languages_array = [];
+
+          for(i in i18nStrings.countries) {
+            languages_array.push( i18nStrings.countries[i] );
+          }
+
+          var languages = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.whitespace,
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            local: languages_array
+          });
+
+          var typeahead = $(fieldset).find('.typeahead');
+
+          typeahead.typeahead({
+            hint: false,
+            highlight: true,
+            minLength: 0
+          },{
+            name: 'languages',
+            limit: 9999,
+            source: function(q, sync){
+              if (q === '') {
+                sync(languages_array);
+              }
+              else {
+                languages.search(q, sync);
+              }
+            }
+          });
+
+          dropdown_parent.on('shown.bs.dropdown', function(){
+            // hack to initially open the typeahead suggestion list
+            typeahead.typeahead('val', 'x');
+            typeahead.typeahead('val', '');
+          });
+
+          typeahead.bind('typeahead:select', function(e, suggestion) {
+            language_button.dropdown('toggle');
+            typeahead.typeahead('val', '');
+            language_button.find('.text').text(suggestion);
+            language_input.val(suggestion);
+          });
+
         });
       });
 
     }
   };
 
-  Hijax.behaviours.localizedInput = my;
+  //Hijax.behaviours.localizedInput = my;
   return Hijax;
 
 })(jQuery, Hijax);
