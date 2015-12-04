63fe623da84203c96066a5e5d6883e1b7716ad64
Felix Ostrowski <felix.ostrowski@googlemail.com>
2015-12-04 19:33:58 +0100

    Add JS stringFormat helper

--- public/javascripts/helpers/client.js
+++ public/javascripts/helpers/client.js
@@ -2,3 +2,19 @@ Handlebars.registerHelper('i18n', function (key, options) {
   var bundle = options.hash.bundle || 'messages';
   return new Handlebars.SafeString(i18nStrings[bundle][key] || key);
 });
+
+Handlebars.registerHelper('stringFormat', function() {
+
+  var args = Array.prototype.slice.call(arguments);
+  var format = args[0];
+  var params = args.slice(1, args.length - 1);
+  var options = args[args.length - 1];
+
+  var size = format.match(/%./g).length;
+  for (var i = 0; i < size; i++) {
+    format = format.replace(/%./, params[i]);
+  }
+
+  return new Handlebars.SafeString(format);
+
+});
