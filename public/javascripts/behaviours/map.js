3a8d2fe2ea8f1c94506e3d1abc46cc052b1ed1e2
joh <j@sights.de>
2016-02-09 17:20:37 +0100

    Repair pin highlighting on list hover, fixes #425

--- public/javascripts/behaviours/map.js
+++ public/javascripts/behaviours/map.js
@@ -289,10 +289,6 @@ var Hijax = (function ($, Hijax) {
       return;
     }
 
-    // console.log(clusterLayer, feature);
-
-    // clusterLayer.getSource().removeFeature(feature);
-
     feature.setStyle(
       styles[ "placemark_cluster" ][ style ](feature)
     );
@@ -580,7 +576,7 @@ var Hijax = (function ($, Hijax) {
 
         var feature = new ol.Feature(featureProperties);
         feature.setId(resource['@id']);
-        feature.setStyle(styles['placemark']['base']());
+        feature.setStyle(styles['placemark']['base']()); // ... seems not to be necessary
         _markers.push(feature);
       }
     }
@@ -869,47 +865,75 @@ var Hijax = (function ($, Hijax) {
       });
 
       // Link list entries to pins
+      // ... quite lengthy. Could need some refactoring. Probably by capsulating the resource/pin connection.
       $('[data-behaviour~="linkedListEntries"]', context).each(function(){
         $( this ).on("mouseenter", "li", function() {
 
           var id = this.getAttribute("about");
-          //var script = $(this).closest("ul").children('script[type="application/ld+json"]');
+          var script = $(this).closest("ul").children('script[type="application/ld+json"]');
 
-          if (true) {
+          if (script.length) {
 
-            // find cluster containing feature with given id
-            var cluster;
-            clusterLayer.getSource().forEachFeature(function(f){
-              var cfeatures = f.get('features');
-              for(var j = 0; j < cfeatures.length; j++) {
-                if(id == cfeatures[j].getId()) {
-                  cluster = f;
+            // first get the markers that represent hovered resource
+            var json = JSON.parse( script.html() );
+            var resource = json.filter(function(resource) {
+              return resource['@id'] == id;
+            })[0];
+            var markers = getMarkers(resource);
+
+            // iterate over markers and collect the clusters, they are in
+            var clusters = [];
+            for(var i = 0; i < markers.length; i++) {
+              clusterLayer.getSource().forEachFeature(function(f){
+                var cfeatures = f.get('features');
+                for(var j = 0; j < cfeatures.length; j++) {
+                  if(markers[i].getId() == cfeatures[j].getId()) {
+                    clusters.push(f);
+                  }
                 }
-              }
-            });
+              });
+            }
 
-            // set hover style
-            if(cluster) {
-              cluster.setStyle(styles.placemark_cluster.hover(cluster));
+            // last but not least, change the style of the clusters, we found
+            for(var i = 0; i < clusters.length; i++) {
+              clusters[i].setStyle(styles.placemark_cluster.hover(clusters[i]));
             }
 
           }
         });
         $( this ).on("mouseleave", "li", function() {
+
           var id = this.getAttribute("about");
           var script = $(this).closest("ul").children('script[type="application/ld+json"]');
+
           if (script.length) {
+
+            // first get the markers that represent hovered resource
             var json = JSON.parse( script.html() );
             var resource = json.filter(function(resource) {
               return resource['@id'] == id;
             })[0];
-            //var markers = getMarkers(resource);
-/*
-            for (var i = 0; i < markers.length; i++) {
-              markers[i].setStyle(styles.placemark.base());
+            var markers = getMarkers(resource);
+
+            // iterate over markers and collect the clusters, they are in
+            var clusters = [];
+            for(var i = 0; i < markers.length; i++) {
+              clusterLayer.getSource().forEachFeature(function(f){
+                var cfeatures = f.get('features');
+                for(var j = 0; j < cfeatures.length; j++) {
+                  if(markers[i].getId() == cfeatures[j].getId()) {
+                    clusters.push(f);
+                  }
+                }
+              });
             }
-*/
+
+            // last but not least, change the style of the clusters, we found
+            for(var i = 0; i < clusters.length; i++) {
+              clusters[i].setStyle(styles.placemark_cluster.base(clusters[i]));
+            }
           }
+
         });
       });
 
