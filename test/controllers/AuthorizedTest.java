bfec3724b1a92b5a7f428a715967c96bebad8ba0
Felix Ostrowski <felix.ostrowski@googlemail.com>
2016-03-11 19:05:28 +0100

    Remove authentification, to be handled by Apache

--- test/controllers/AuthorizedTest.java
+++ test/controllers/AuthorizedTest.java
@@ -21,18 +21,6 @@ import static play.test.Helpers.status;
 public class AuthorizedTest extends ElasticsearchTestGrid {
 
   @Test
-  public void notAuthenticated() {
-
-    running(fakeApplication(), new Runnable() {
-      @Override public void run() {
-        Result result = route(fakeRequest("GET", routes.UserIndex.manageToken().url()));
-        assertEquals(401, status(result));
-      }
-    });
-
-  }
-
-  @Test
   public void authenticated() {
 
     final Resource user = new Resource("Person");
@@ -45,37 +33,10 @@ public class AuthorizedTest extends ElasticsearchTestGrid {
         String authString = email + ":" + token;
         String auth = Base64.getEncoder()
           .encodeToString(authString.getBytes());
-        Result result = route(fakeRequest("GET", routes.UserIndex.manageToken().url())
+        Result result = route(fakeRequest("GET", routes.LandingPage.get().url())
           .withHeader("Authorization", "Basic " + auth));
         assertEquals(200, status(result));
         Account.removeTokenFor(user);
-      }
-    });
-
-  }
-
-  @Test
-  public void logOut() {
-
-    final Resource user = new Resource("Person");
-    final String email = "foo@bar.de";
-    user.put("email", email);
-
-    running(fakeApplication(), new Runnable() {
-      @Override public void run() {
-        String token = Account.createTokenFor(user);
-        String authString = email + ":" + token;
-        String auth = Base64.getEncoder()
-          .encodeToString(authString.getBytes());
-        Result result = route(fakeRequest("GET", routes.UserIndex.manageToken().url())
-          .withHeader("Authorization", "Basic " + auth));
-        assertEquals(200, status(result));
-        result = route(fakeRequest("POST", routes.UserIndex.deleteToken().url())
-          .withHeader("Authorization", "Basic " + auth));
-        assertEquals(200, status(result));
-        result = route(fakeRequest("GET", routes.UserIndex.manageToken().url())
-          .withHeader("Authorization", "Basic " + auth));
-        assertEquals(401, status(result));
       }
     });
 
