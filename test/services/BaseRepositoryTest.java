7586589681f2f90411e397fc58e751134ad7c328
Philipp v. Böselager <boeselager@hbz-nrw.de>
2016-04-13 16:54:30 +0200

    Fix BaseRepositoryTest s. Not finished yet.

--- test/services/BaseRepositoryTest.java
+++ test/services/BaseRepositoryTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -41,7 +40,7 @@ public class BaseRepositoryTest extends ElasticsearchTestGrid implements JsonTes
   }
 
   @Test
-  public void testDeleteResourceWithMentionedResources() throws IOException {
+  public void testDeleteResourceWithMentionedResources() throws IOException, InterruptedException {
     // setup: 1 Person ("in1") who has 2 affiliations
     Resource in = getResourceFromJsonFile("BaseRepositoryTest/testDeleteResourceWithMentionedResources.IN.1.json");
     Resource expected1 = getResourceFromJsonFile(
@@ -53,6 +52,9 @@ public class BaseRepositoryTest extends ElasticsearchTestGrid implements JsonTes
     // delete affiliation "Oh No Company" and check whether it has been removed
     // from referencing resources
     Resource toBeDeleted = mBaseRepo.getResource("info:urn:uuid:49d8b330-e3d5-40ca-b5cb-2a8dfca70987");
+    // FIXME: Thread.sleep to be deleted when Repo synchronization is
+    // triggerable
+    Thread.sleep(1000);
     mBaseRepo.deleteResource(toBeDeleted.getAsString(JsonLdConstants.ID));
     Resource result1 = mBaseRepo.getResource("info:urn:uuid:49d8b330-e3d5-40ca-b5cb-2a8dfca70456");
     Resource result2 = mBaseRepo.getResource("info:urn:uuid:49d8b330-e3d5-40ca-b5cb-2a8dfca70123");
@@ -61,24 +63,23 @@ public class BaseRepositoryTest extends ElasticsearchTestGrid implements JsonTes
     Assert.assertNull(mBaseRepo.getResource("info:urn:uuid:49d8b330-e3d5-40ca-b5cb-2a8dfca70987"));
   }
 
-  // FIXME: Fix when @context being ingested is solved, also URIs must be used
-  // as IDs
-  // @Test
-  public void testDeleteLastResourceInList() throws IOException {
+  @Test
+  public void testDeleteLastResourceInList() throws IOException, InterruptedException {
     Resource db1 = getResourceFromJsonFile("BaseRepositoryTest/testDeleteLastResourceInList.DB.1.json");
     Resource db2 = getResourceFromJsonFile("BaseRepositoryTest/testDeleteLastResourceInList.DB.2.json");
     Resource out = getResourceFromJsonFile("BaseRepositoryTest/testDeleteLastResourceInList.OUT.1.json");
     mBaseRepo.addResource(db1, new HashMap<>());
     mBaseRepo.addResource(db2, new HashMap<>());
+    // FIXME: Thread.sleep to be deleted when Repo synchronization is
+    // triggerable
+    Thread.sleep(1000);
     mBaseRepo.deleteResource("urn:uuid:3a25e950-a3c0-425d-946d-9806665ec665");
     Assert.assertNull(mBaseRepo.getResource("urn:uuid:3a25e950-a3c0-425d-946d-9806665ec665"));
     Assert.assertEquals(out, mBaseRepo.getResource("urn:uuid:c7f5334a-3ddb-4e46-8653-4d8c01e25503"));
   }
 
-  // FIXME: Fix when @context being ingested is solved, also URIs must be used
-  // as IDs
-  // @Test
-  public void testDeleteResourceFromList() throws IOException {
+  @Test
+  public void testDeleteResourceFromList() throws IOException, InterruptedException {
     Resource db1 = getResourceFromJsonFile("BaseRepositoryTest/testDeleteResourceFromList.DB.1.json");
     Resource db2 = getResourceFromJsonFile("BaseRepositoryTest/testDeleteResourceFromList.DB.2.json");
     Resource db3 = getResourceFromJsonFile("BaseRepositoryTest/testDeleteResourceFromList.DB.2.json");
@@ -87,15 +88,16 @@ public class BaseRepositoryTest extends ElasticsearchTestGrid implements JsonTes
     mBaseRepo.addResource(db1, new HashMap<>());
     mBaseRepo.addResource(db2, new HashMap<>());
     mBaseRepo.addResource(db3, new HashMap<>());
+    // FIXME: Thread.sleep to be deleted when Repo synchronization is
+    // triggerable
+    Thread.sleep(1000);
     mBaseRepo.deleteResource("urn:uuid:3a25e950-a3c0-425d-946d-9806665ec665");
     Assert.assertNull(mBaseRepo.getResource("urn:uuid:3a25e950-a3c0-425d-946d-9806665ec665"));
     Assert.assertEquals(out1, mBaseRepo.getResource("urn:uuid:c7f5334a-3ddb-4e46-8653-4d8c01e25503"));
     Assert.assertEquals(out2, mBaseRepo.getResource("urn:uuid:7cfb9aab-1a3f-494c-8fb1-64755faf180c"));
   }
 
-  // FIXME: Fix when @context being ingested is solved, also URIs must be used
-  // as IDs
-  // @Test
+  @Test
   public void testRemoveReference() throws IOException {
     Resource in = getResourceFromJsonFile("BaseRepositoryTest/testRemoveReference.IN.json");
     Resource db1 = getResourceFromJsonFile("BaseRepositoryTest/testRemoveReference.DB.1.json");
