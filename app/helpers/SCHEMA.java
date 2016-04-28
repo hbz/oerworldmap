5ec04ec5e6a9fd517a713b2a22bd2ab3a789c3fb
Felix Ostrowski <felix.ostrowski@googlemail.com>
2016-04-28 17:40:33 +0200

    Do not trigger indexing for schema:about

---
+++ app/helpers/SCHEMA.java
@@ -0,0 +1,32 @@
+package helpers;
+
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
+
+/**
+ * Created by fo on 28.04.16.
+ * The schema.org vocabulary
+ */
+public class SCHEMA {
+
+  protected static final String uri ="http://schema.org/";
+
+  /** returns the URI for this schema
+   @return the URI for this schema
+   */
+  public static String getURI() {
+    return uri;
+  }
+
+  protected static final Resource resource(String local ) {
+    return ResourceFactory.createResource( uri + local );
+  }
+
+  protected static final Property property(String local ) {
+    return ResourceFactory.createProperty( uri, local );
+  }
+
+  public static final Property about = property("about");
+
+}
