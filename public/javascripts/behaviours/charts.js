bdec09e5e9df5bd651a1affb20f8ed90fdbce529
Felix Ostrowski <felix.ostrowski@googlemail.com>
2016-06-16 13:15:45 +0200

    Revert "Initial refactoring of charts as JS behaviour"
    This reverts commit b807ca64fd8c4817a465e59ebff25f7e97910995.
    
    We will switch to client side rendering of charts once we have more use cases
    for different chart types.

--- public/javascripts/behaviours/charts.js
+++ public/javascripts/behaviours/charts.js
@@ -1,41 +1,30 @@
 var Hijax = (function ($, Hijax) {
 
   var my = {
-
+    
     attach: function(context) {
 
-      $('[data-behaviour="chart-pie"]', context).each(function() {
-        var json = JSON.parse( $(this).find('script[type="application/ld+json"]').html() );
-        var data = {
-          labels: json.buckets.map(function(bucket) {
-            return bucket.key
-          }),
-          datasets: [
-            {
-              data: json.buckets.map(function(bucket) {
-                return bucket.doc_count
-              }),
-              backgroundColor: json.buckets.map(function(bucket) {
-                return '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6)
-              })
-            }
-          ]
-        };
-        $(this).hide();
-        var canvas = $('<canvas width="400" height="400"></canvas>');
-        $(this).before(canvas);
-        var chart = new Chart(canvas, {
-            type: 'pie',
-            data: data,
-            options: {
-              responsive: false
-            }
-        });
+      $(window).resize(function(){
+        my.scaleCharts();
       });
-
+      
     },
-
-
+    
+    layout: function() {
+      my.scaleCharts();
+    },
+    
+    scaleCharts: function() {
+      
+      $('svg.chart').each(function(){
+        $(this).css({
+    			width: $(this).parent().width(),
+    			height: $(this).parent().width() * ( this.getAttribute("width") / this.getAttribute("height") )
+    		});
+      });
+      
+    }
+    
   }
 
   Hijax.behaviours.charts = my;
