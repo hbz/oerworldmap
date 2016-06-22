597a9869bfce919803c660528867c50db2cfe83c
Felix Ostrowski <felix.ostrowski@googlemail.com>
2016-06-22 16:43:43 +0200

    Add recent additions & global stats to splash page

--- app/controllers/LandingPage.java
+++ app/controllers/LandingPage.java
@@ -1,13 +1,29 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import models.Resource;
+import models.ResourceList;
 import play.mvc.Result;
+import services.AggregationProvider;
+import services.QueryContext;
 
 public class LandingPage extends OERWorldMap {
 
   public static Result get() throws IOException {
 
-    return ok(render("OER World Map", "LandingPage/index.mustache"));
+    QueryContext queryContext = (QueryContext) ctx().args.get("queryContext");
+    ResourceList resourceList = mBaseRepository.query("", 0, 5, "dateCreated:DESC", null, queryContext);
+    Resource typeAggregation = mBaseRepository.aggregate(AggregationProvider.getTypeAggregation(0));
+
+    Map<String, Object> scope = new HashMap<>();
+    scope.put("resources", resourceList.toResource());
+    scope.put("typeAggregation", typeAggregation);
+
+    return ok(render("OER World Map", "LandingPage/index.mustache", scope));
 
   }
 
