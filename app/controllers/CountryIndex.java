hbz/oerworldmap

commit 06ce8e758e515ace2ab48d5c8165a7e557a30cf8

Add asterisk autocompletion for search terms longer than 2 characters

app/services/repository/ElasticsearchRepository.java
@@ -374,7 +374,9 @@ public class ElasticsearchRepository extends Repository implements Readable, Wri
 
     QueryBuilder queryBuilder;
     if (!StringUtils.isEmpty(aQueryString)) {
-      queryBuilder = QueryBuilders.queryString(aQueryString).fuzziness(mFuzziness);
+      String queryString = (aQueryString.length() > 2 && !aQueryString.endsWith("*")) ?
+        aQueryString + "*" : aQueryString;
+      queryBuilder = QueryBuilders.queryString(queryString).fuzziness(mFuzziness);
       if (fieldBoosts != null) {
         for (String fieldBoost : fieldBoosts) {
           try {

test/services/BaseRepositoryTest.java
@@ -390,7 +390,6 @@ public class BaseRepositoryTest extends ElasticsearchTestGrid implements JsonTes
   public void testSearchFuzzyExtension() throws IOException, InterruptedException {
     Resource db1 = getResourceFromJsonFile("BaseRepositoryTest/testSearchFuzzyExtension.DB.1.json");
     mBaseRepo.addResource(db1, mMetadata);
-    System.out.println(db1.toString());
 
     // FIXME: Thread.sleep to be deleted when Repo synchronization is
     // triggerable
@@ -414,7 +413,6 @@ public class BaseRepositoryTest extends ElasticsearchTestGrid implements JsonTes
   public void testSearchFuzzyDiacritica() throws IOException, InterruptedException {
     Resource db1 = getResourceFromJsonFile("BaseRepositoryTest/testSearchFuzzyDiacritica.DB.1.json");
     mBaseRepo.addResource(db1, mMetadata);
-    System.out.println(db1.toString());
 
     // FIXME: Thread.sleep to be deleted when Repo synchronization is
     // triggerable
@@ -432,6 +430,40 @@ public class BaseRepositoryTest extends ElasticsearchTestGrid implements JsonTes
 
     mBaseRepo.deleteResource("urn:uuid:9843bac3-028f-4be8-ac54-92dcfeb00001", mMetadata);
     mBaseRepo.deleteResource("urn:uuid:9843bac3-028f-4be8-ac54-92dcfeb00002", mMetadata);
+  }
+
+  @Test
+  public void testSearchTermAppendedByAsterisk() throws IOException, InterruptedException {
+    Resource db1 = getResourceFromJsonFile("BaseRepositoryTest/testSearchTermAppendedByAsterisk.DB.1.json");
+    mBaseRepo.addResource(db1, mMetadata);
+
+    // FIXME: Thread.sleep to be deleted when Repo synchronization is
+    // triggerable
+    Thread.sleep(5000);
+    QueryContext queryContext = new QueryContext(null);
+    queryContext.setElasticsearchFieldBoosts(new SearchConfig().getBoostsForElasticsearch());
+
+    // query whole term
+    List<Resource> wholeterm = mBaseRepo.query("weareoer", 0, 10, null, null, queryContext).getItems();
+    Assert.assertTrue("Could not find \"weareoer\".", wholeterm.size() == 1);
+
+    // query two characters
+    List<Resource> twochars = mBaseRepo.query("we", 0, 10, null, null, queryContext).getItems();
+    Assert.assertTrue("Found unintended result for \"we\".", twochars.size() == 0);
+
+    // query two characters plus "explicit" asterisk
+    List<Resource> twocharsasterisk = mBaseRepo.query("we*", 0, 10, null, null, queryContext).getItems();
+    Assert.assertTrue("Could not find result for \"we*\".", twocharsasterisk.size() == 1);
+
+    // query three characters (automatically completed with asterisk)
+    List<Resource> threechars = mBaseRepo.query("wea", 0, 10, null, null, queryContext).getItems();
+    Assert.assertTrue("Could not find result for \"wea\".", threechars.size() == 1);
+
+    // query three characters plus "explicit" asterisk
+    List<Resource> threecharsasterisk = mBaseRepo.query("wea*", 0, 10, null, null, queryContext).getItems();
+    Assert.assertTrue("Could not find result for \"wea*\".", threecharsasterisk.size() == 1);
+
+    mBaseRepo.deleteResource("urn:uuid:9843bac3-028f-4be8-ac54-92waoer00001", mMetadata);
   }
 
   private List<String> getNameList(List<Resource> aResourceList) {

commit 1d2ae9f1245854daecde3d5c56e350e8732542c2

Fix country listings
06ce8e758e515ace2ab48d5c8165a7e557a30cf8 broke the current query, using filter
instead now.

app/controllers/CountryIndex.java
@@ -3,8 +3,10 @@ package controllers;
 import helpers.Countries;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -39,11 +41,16 @@ public class CountryIndex extends OERWorldMap {
     });
 
     Resource countryAggregation = mBaseRepository.aggregate(AggregationProvider.getForCountryAggregation(id.toUpperCase(), 0));
-    ResourceList champions = mBaseRepository.query(
-        Record.RESOURCE_KEY + ".countryChampionFor:".concat(id.toUpperCase()), 0, 9999, null, null);
-    ResourceList resources = mBaseRepository.query(
-        Record.RESOURCE_KEY + ".location.address.addressCountry:".concat(id.toUpperCase()), 0, 9999, null, null,
-            queryContext);
+
+    Map<String, List<String>> filters = new HashMap<>();
+    // FIXME: update mapping of countryChampionFor to not analyzed, use upper case here
+    filters.put(Record.RESOURCE_KEY + ".countryChampionFor", Arrays.asList(id.toLowerCase()));
+    ResourceList champions = mBaseRepository.query("*", 0, 9999, null, filters);
+
+    filters.clear();
+    filters.put(Record.RESOURCE_KEY + ".location.address.addressCountry", Arrays.asList(id.toUpperCase()));
+    ResourceList resources = mBaseRepository.query("*", 0, 9999, null, filters, queryContext);
+
     Map<String, Object> scope = new HashMap<>();
 
     scope.put("alpha-2", id.toUpperCase());

app/controllers/ResourceIndex.java
@@ -45,7 +45,7 @@ public class ResourceIndex extends OERWorldMap {
       throws IOException, ParseException {
 
     // Extract filters directly from query params
-    Map<String, ArrayList<String>> filters = new HashMap<>();
+    Map<String, List<String>> filters = new HashMap<>();
     Pattern filterPattern = Pattern.compile("^filter\\.(.*)$");
     for (Map.Entry<String, String[]> entry : request().queryString().entrySet()) {
       Matcher filterMatcher = filterPattern.matcher(entry.getKey());

app/models/ResourceList.java
@@ -4,7 +4,6 @@ import org.apache.commons.lang3.StringUtils;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -25,12 +24,12 @@ public class ResourceList {
 
   private int offset;
 
-  private Map<String, ArrayList<String>> filters;
+  private Map<String, List<String>> filters;
 
   private Resource aggregations;
 
   public ResourceList(@Nonnull List<Resource> aResourceList, long aTotalItems, String aSearchTerms, int aOffset,
-                      int aSize, String aSortOrder, Map<String, ArrayList<String>> aFilters, Resource aAggregations) {
+                      int aSize, String aSortOrder, Map<String, List<String>> aFilters, Resource aAggregations) {
     items = aResourceList;
     totalItems = aTotalItems;
     searchTerms = aSearchTerms;
@@ -70,7 +69,7 @@ public class ResourceList {
       params.add("sort=".concat(sortOrder));
     }
     if (!(null == filters)) {
-      for (Map.Entry<String, ArrayList<String>> entry : filters.entrySet()) {
+      for (Map.Entry<String, List<String>> entry : filters.entrySet()) {
         for (String filter : entry.getValue()) {
           params.add("filter.".concat(entry.getKey()).concat("=").concat(filter));
         }
@@ -95,7 +94,7 @@ public class ResourceList {
       params.add("sort=".concat(sortOrder));
     }
     if (!(null == filters)) {
-      for (Map.Entry<String, ArrayList<String>> entry : filters.entrySet()) {
+      for (Map.Entry<String, List<String>> entry : filters.entrySet()) {
         for (String filter : entry.getValue()) {
           params.add("filter.".concat(entry.getKey()).concat("=").concat(filter));
         }
@@ -121,7 +120,7 @@ public class ResourceList {
       params.add("sort=".concat(sortOrder));
     }
     if (!(null == filters)) {
-      for (Map.Entry<String, ArrayList<String>> entry : filters.entrySet()) {
+      for (Map.Entry<String, List<String>> entry : filters.entrySet()) {
         for (String filter : entry.getValue()) {
           params.add("filter.".concat(entry.getKey()).concat("=").concat(filter));
         }
@@ -146,7 +145,7 @@ public class ResourceList {
       params.add("sort=".concat(sortOrder));
     }
     if (!(null == filters)) {
-      for (Map.Entry<String, ArrayList<String>> entry : filters.entrySet()) {
+      for (Map.Entry<String, List<String>> entry : filters.entrySet()) {
         for (String filter : entry.getValue()) {
           params.add("filter.".concat(entry.getKey()).concat("=").concat(filter));
         }
@@ -175,7 +174,7 @@ public class ResourceList {
       params.add("sort=".concat(sortOrder));
     }
     if (!(null == filters)) {
-      for (Map.Entry<String, ArrayList<String>> entry : filters.entrySet()) {
+      for (Map.Entry<String, List<String>> entry : filters.entrySet()) {
         for (String filter : entry.getValue()) {
           params.add("filter.".concat(entry.getKey()).concat("=").concat(filter));
         }

app/services/repository/BaseRepository.java
@@ -211,12 +211,12 @@ public class BaseRepository extends Repository
 
   @Override
   public ResourceList query(@Nonnull String aQueryString, int aFrom, int aSize, String aSortOrder,
-                            Map<String, ArrayList<String>> aFilters) {
+                            Map<String, List<String>> aFilters) {
     return query(aQueryString, aFrom, aSize, aSortOrder, aFilters, null);
   }
 
   public ResourceList query(@Nonnull String aQueryString, int aFrom, int aSize, String aSortOrder,
-      Map<String, ArrayList<String>> aFilters, QueryContext aQueryContext) {
+                            Map<String, List<String>> aFilters, QueryContext aQueryContext) {
     ResourceList resourceList;
     try {
       resourceList = mElasticsearchRepo.query(aQueryString, aFrom, aSize, aSortOrder, aFilters, aQueryContext);

app/services/repository/ElasticsearchRepository.java
@@ -164,12 +164,12 @@ public class ElasticsearchRepository extends Repository implements Readable, Wri
    */
   @Override
   public ResourceList query(@Nonnull String aQueryString, int aFrom, int aSize, String aSortOrder,
-                            Map<String, ArrayList<String>> aFilters) throws IOException, ParseException {
+                            Map<String, List<String>> aFilters) throws IOException, ParseException {
     return query(aQueryString, aFrom, aSize, aSortOrder, aFilters, null);
   }
 
   public ResourceList query(@Nonnull String aQueryString, int aFrom, int aSize, String aSortOrder,
-                            Map<String, ArrayList<String>> aFilters, QueryContext aQueryContext) throws IOException, ParseException {
+                            Map<String, List<String>> aFilters, QueryContext aQueryContext) throws IOException, ParseException {
 
     SearchResponse response = esQuery(aQueryString, aFrom, aSize, aSortOrder, aFilters, aQueryContext);
     Iterator<SearchHit> searchHits = response.getHits().iterator();
@@ -298,7 +298,7 @@ public class ElasticsearchRepository extends Repository implements Readable, Wri
   }
 
   private SearchResponse esQuery(@Nonnull String aQueryString, int aFrom, int aSize, String aSortOrder,
-                                 Map<String, ArrayList<String>> aFilters, QueryContext aQueryContext) {
+                                 Map<String, List<String>> aFilters, QueryContext aQueryContext) {
 
     SearchRequestBuilder searchRequestBuilder = mClient.prepareSearch(mConfig.getIndex());
 
@@ -343,7 +343,7 @@ public class ElasticsearchRepository extends Repository implements Readable, Wri
 
     if (!(null == aFilters)) {
       AndFilterBuilder aggregationAndFilter = FilterBuilders.andFilter();
-      for (Map.Entry<String, ArrayList<String>> entry : aFilters.entrySet()) {
+      for (Map.Entry<String, List<String>> entry : aFilters.entrySet()) {
         // This could also be an AndFilterBuilder allowing to narrow down the result list
         OrFilterBuilder orFilterBuilder = FilterBuilders.orFilter();
         for (String filter : entry.getValue()) {

app/services/repository/Queryable.java
@@ -1,7 +1,7 @@
 package services.repository;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Nonnull;
@@ -17,7 +17,7 @@ public interface Queryable {
 
   /**
    * Query for resources.
-   * 
+   *
    * @param aQueryString
    *          A string describing the query
    * @param aFilters
@@ -25,6 +25,6 @@ public interface Queryable {
    *         criteria given in the query string
    */
   ResourceList query(@Nonnull String aQueryString, int aFrom, int aSize, String aSortOrder,
-      Map<String, ArrayList<String>> aFilters) throws IOException, ParseException;
+                     Map<String, List<String>> aFilters) throws IOException, ParseException;
 
 }