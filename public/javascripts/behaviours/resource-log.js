cce1267722e9e3233c203009bb0c9b7bf31fcdb2
Felix Ostrowski <felix.ostrowski@googlemail.com>
2016-11-02 14:43:18 +0100

    Show unified diff, add buttons to select versions

--- public/javascripts/behaviours/resource-log.js
+++ public/javascripts/behaviours/resource-log.js
@@ -1,31 +1,37 @@
 var Hijax = (function ($, Hijax) {
 
-  var toHtml;
+  function showDiff(fromURL, toURL, context) {
+    $.get(toURL, function(toData) {
+      $.get(fromURL, function(fromData) {
+        var diff = $(htmldiff($(fromData).filter('main').find('article').html(), $(toData).filter('main').find('article').html()));
+        diff.find('del').css({'background-color': 'rgba(255, 0, 0, 0.5)', 'text-decoration': 'none'});
+        diff.find('ins').css({'background-color': 'rgba(0, 255, 0, 0.5)', 'text-decoration': 'none'});
+        $('#diff', context).html(diff);
+        return;
+      });
+    });
+  }
 
   var my = {
 
     attach: function(context) {
 
-      $.get($('#resource', context).attr('href'), function(data) {
-        toHtml = $(data).filter('main').find('article').html();
-        $('#to', context).html(toHtml);
+      $('input', context).on('change', function() {
+        var from = $('input[name=from]:checked', context).val() || 'HEAD';
+        var to = $('input[name=to]:checked', context).val()  || 'HEAD';
+        window.location.hash = from + '..' + to;
       });
 
-      $('[data-behaviour="resource-log"] pre a', context).bind('click', function() {
-        $.get(this.getAttribute('href'), function(data) {
-          var fromHtml = $(data).filter('main').find('article').html();
-          var from = $(htmldiff(fromHtml, toHtml));
-          from.find('ins').remove();
-          from.find('del').css({'background-color': 'rgba(255, 0, 0, 0.5)', 'text-decoration': 'none'});
-          var to = $(htmldiff(fromHtml, toHtml));
-          to.find('del').remove();
-          to.find('ins').css({'background-color': 'rgba(0, 255, 0, 0.5)', 'text-decoration': 'none'});
-          $('#to', context).html(to);
-          $('#from', context).html(from);
-        });
-        return false;
-      });
+      window.onhashchange = function() {
+        var url = $('#resource', context).attr('href');
+        var range = window.location.hash.substr(1).split("..");
+        var fromURL = url + '?version=' + range[0];
+        var toURL = url + '?version=' + range[1];
+        showDiff(fromURL, toURL, context);
+      }
 
+      $("input:radio[name=from]:eq(1)", context).click();
+      $("input:radio[name=to]:eq(0)", context).click();
 
     }
 
