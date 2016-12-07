4b2c3071e8ff8c9dd35f0241c59b43a08cf07f8a
Felix Ostrowski <felix.ostrowski@googlemail.com>
2016-12-07 16:23:26 +0100

    Fix geo list for providers with multiple services

--- public/javascripts/behaviours/map.js
+++ public/javascripts/behaviours/map.js
@@ -639,7 +639,6 @@ var Hijax = (function ($, Hijax) {
           geometry: point,
           url: "/resource/" + resource['@id'],
           type: resource['@type'],
-          origin_id: origin['@id'],
         };
 
         var feature = new ol.Feature(featureProperties);
@@ -1041,7 +1040,12 @@ var Hijax = (function ($, Hijax) {
             var extent = world.getView().calculateExtent(world.getSize());
             list.children('li').hide();
             placemarksVectorSource.forEachFeatureInExtent(extent, function(feature) {
-              list.children('li[about="' + feature.getProperties()['origin_id'] +'"]').show();
+              var resource = feature.getProperties()['resource'];
+              var ids = resource['referencedBy'] ? resource['referencedBy'].map(function(obj){return obj['@id']}) : [];
+              ids.push(resource['@id']);
+              for (var i = 0; i < ids.length; i++) {
+                list.children('li[about="' + ids[i] + '"]').show();
+              }
             });
           } else {
             list.children('li').show();
