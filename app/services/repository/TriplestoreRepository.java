f446d6064ccb1ccd0b3ce724bea95a389673923f
Felix Ostrowski <felix.ostrowski@googlemail.com>
2016-12-13 16:17:04 +0100

    Add links from intermediate bnodes to descriptions

--- app/services/repository/TriplestoreRepository.java
+++ app/services/repository/TriplestoreRepository.java
@@ -50,9 +50,7 @@ import services.ResourceFramer;
 public class TriplestoreRepository extends Repository implements Readable, Writable, Versionable {
 
   public static final String EXTENDED_DESCRIPTION =
-    "DESCRIBE <%1$s> ?o ?oo WHERE {" +
-    "  <%1$s> ?p ?o OPTIONAL { ?o ?pp ?oo }" +
-    "}";
+    "DESCRIBE <%1$s> ?o ?oo ?ooo WHERE { <%1$s> ?p ?o OPTIONAL { ?o ?pp ?oo OPTIONAL { ?oo ?ppp ?ooo FILTER isBlank(?oo)} } }";
 
   public static final String CONCISE_BOUNDED_DESCRIPTION = "DESCRIBE <%s>";
 
