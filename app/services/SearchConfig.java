0504841a546f5cb5f934b7c862bfb4b3aef3215b
Philipp von Böselager <philipp.boeselager@web.de>
2017-02-20 18:46:51 +0100

    Refactor SearchConfig to prevent multiple configuration reload

--- app/services/SearchConfig.java
+++ app/services/SearchConfig.java
@@ -1,53 +1,38 @@
 package services;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-
-import org.apache.commons.lang3.StringUtils;
-
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import com.typesafe.config.ConfigRenderOptions;
 import com.typesafe.config.ConfigValue;
+
+import javax.annotation.Nonnull;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.Map.Entry;
 
 /**
  * @author pvb
  */
 public class SearchConfig {
 
-  private Set<Entry<String, ConfigValue>> mEntries;
-  private Config mConfig;
+  private Config mConfig = null;
+  private final List<String> mBoosts = new ArrayList<>();
   private static final String DEFAULT_CONFIG_FILE = "conf/search.conf";
+  private static Config mDefaultConfig = ConfigFactory.parseFile(new File(DEFAULT_CONFIG_FILE)).resolve();
+  private static final List<String> mDefaultBoosts = new ArrayList<>();
 
-  public SearchConfig() {
-    this(DEFAULT_CONFIG_FILE);
-  }
-
-  public SearchConfig(String aConfigFile) {
-    File configFile;
-    if (!StringUtils.isEmpty(aConfigFile)) {
-      configFile = new File(aConfigFile);
-    } else {
-      configFile = new File(DEFAULT_CONFIG_FILE);
+  public SearchConfig(){
+    if (mDefaultBoosts.size() == 0) {
+      calculateBoostFromConfig(mDefaultBoosts, mDefaultConfig);
     }
-    init(configFile);
   }
 
-  private void init(File aConfigFile) {
-    // CONFIG FILE
-    checkFileExists(aConfigFile);
-    mConfig = ConfigFactory.parseFile(aConfigFile).resolve();
-    init();
-  }
-
-  private void init() {
-    mEntries = mConfig.entrySet();
+  public SearchConfig(@Nonnull String aConfigFile) {
+    File configFile = new File(aConfigFile);
+    checkFileExists(configFile);
+    mConfig = ConfigFactory.parseFile(configFile).resolve();
+    calculateBoostFromConfig(mBoosts, mConfig);
   }
 
   private void checkFileExists(File aFile) {
@@ -60,26 +45,40 @@ public class SearchConfig {
     }
   }
 
-  public Map<String, Double> getBoosts() {
+  private Map<String, Double> getBoosts(Config aConfig) {
     Map<String, Double> result = new HashMap<>();
-    for (Entry<String, ConfigValue> entry : mEntries) {
+    for (Entry<String, ConfigValue> entry : aConfig.entrySet()) {
       if (entry.getKey().startsWith("\"boost:")) {
         String key = entry.getKey().replaceAll("\"", "").substring(6);
-        Double value = Double.valueOf(((ConfigValue) entry.getValue()).render(ConfigRenderOptions.defaults()));
+        Double value = Double.valueOf((entry.getValue()).render(ConfigRenderOptions.defaults()));
         result.put(key, value);
       }
     }
     return result;
   }
 
-  public String[] getBoostsForElasticsearch() {
-    List<String> result = new ArrayList<>();
-    Map<String, Double> boostMap = getBoosts();
-    for (Map.Entry<String, Double> boost : boostMap.entrySet()) {
-      StringBuilder esboost = new StringBuilder(boost.getKey());
-      esboost.append("^").append(boost.getValue());
-      result.add(esboost.toString());
+  public void calculateBoostFromConfig() {
+    if (null != mConfig) {
+      calculateBoostFromConfig(mBoosts, mConfig);
     }
-    return result.toArray(new String[0]);
+    else {
+      calculateBoostFromConfig(mDefaultBoosts, mDefaultConfig);
+    }
   }
+
+  private void calculateBoostFromConfig(List<String> aBoosts, Config aConfig) {
+    aBoosts.clear();
+    Map<String, Double> boostMap = getBoosts(aConfig);
+    for (Map.Entry<String, Double> boost : boostMap.entrySet()) {
+      aBoosts.add(boost.getKey() + "^" + boost.getValue());
+    }
+  }
+
+  public String[] getBoostsForElasticsearch(){
+    if (!mBoosts.isEmpty()) {
+      return mBoosts.toArray(new String[0]);
+    }
+    return mDefaultBoosts.toArray(new String[0]);
+  }
+
 }
