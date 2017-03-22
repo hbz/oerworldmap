2e272eedd79cbf9f6e6ebd8685287eebb85e3518
joh <j@sights.de>
2017-03-22 18:12:43 +0100

    fix #892 (unset addressRegion, if country "None")

--- public/javascripts/behaviours/resource-form-widgets/place.js
+++ public/javascripts/behaviours/resource-form-widgets/place.js
@@ -143,6 +143,9 @@ var Hijax = (function ($, Hijax) {
           widget.find('[name="location[address][addressCountry]"]').val(
             suggestion.id
           );
+          if(! suggestion.id) {
+            widget.find('[name="location[address][addressRegion]"]').val('');
+          }
           country_dropdown_button.find('.text').text(
             suggestion.label
           );
