ee5c9beeb4fe7e70c928d31d5e4362210ff558fe
Felix Ostrowski <felix.ostrowski@googlemail.com>
2017-03-28 14:33:46 +0200

    Fix name and inherited locations in GeoJSON

--- app/services/export/GeoJsonExporter.java
+++ app/services/export/GeoJsonExporter.java
@@ -36,44 +36,62 @@ public class GeoJsonExporter implements Exporter {
 
   private JsonNode toGeoJson(Resource aResource) {
 
-    Resource resource = aResource.getAsResource(Record.RESOURCE_KEY);
-    Resource location = resource.getAsResource("location");
-    if (location == null || location.getAsResource("geo") == null) {
-      String[] traverse = new String[] {"mentions", "member", "agent", "participant", "provider"};
-      for (String property : traverse) {
-        Resource ref = aResource.getAsResource(property);
-        if (ref != null && ref.getAsResource("location") != null
-            && ref.getAsResource("location").getAsResource("geo") != null) {
-          location = ref.getAsResource("location");
-          break;
-        }
-      }
-    }
+    JsonNode resource = aResource.getAsResource(Record.RESOURCE_KEY).toJson();
+    ArrayNode coordinates = getCoordinates(resource);
 
-    if (location == null || location.getAsResource("geo") == null) {
+    if (coordinates == null) {
       return null;
     }
 
     ObjectNode node = new ObjectNode(JsonNodeFactory.instance);
     ObjectNode geometry = new ObjectNode(JsonNodeFactory.instance);
-    ArrayNode coordinates = new ArrayNode(JsonNodeFactory.instance);
     ObjectNode properties = new ObjectNode(JsonNodeFactory.instance);
-    coordinates.add(Double.valueOf(location.getAsResource("geo").getAsString("lon")));
-    coordinates.add(Double.valueOf(location.getAsResource("geo").getAsString("lat")));
     node.put("type", "Feature");
     node.set("geometry", geometry);
     node.set("properties", properties);
-    node.put("id", resource.getId());
+    node.set("id", resource.get("@id"));
     geometry.put("type", "Point");
     geometry.set("coordinates", coordinates);
-    properties.put("@id", resource.getId());
-    properties.put("@type", resource.getType());
-    if (resource.getAsResource("name") != null) {
-      properties.set("name", resource.getAsResource("name").toJson());
-    } else {
-      properties.put("name", resource.getId());
+    properties.set("@id", resource.get("@id"));
+    properties.set("@type", resource.get("@type"));
+    if (resource.has("name")) {
+      properties.set("name", resource.get("name"));
     }
     return node;
+
+  }
+
+  private ArrayNode getCoordinates(JsonNode node) {
+
+    String[] traverse = new String[] {"mentions", "member", "agent", "participant", "provider"};
+
+    if (node.isArray()) {
+      for (JsonNode entry : node) {
+        ArrayNode coordinates = getCoordinates(entry);
+        if (coordinates != null) {
+          return coordinates;
+        }
+      }
+    } else if (node.isObject()) {
+      if (node.has("location") && node.get("location").has("geo")) {
+        ArrayNode result = new ArrayNode(JsonNodeFactory.instance);
+        ObjectNode geo = (ObjectNode) node.get("location").get("geo");
+        result.add(geo.get("lon").asDouble());
+        result.add(geo.get("lat").asDouble());
+        return result;
+      } else {
+        for (String property : traverse) {
+          if (node.has(property)) {
+            ArrayNode ref = getCoordinates(node.get(property));
+            if (ref != null) {
+              return ref;
+            }
+          }
+        }
+      }
+    }
+
+    return null;
 
   }
 
