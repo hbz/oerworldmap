0a7e2d6c6c3f9381fc17214df04eaf5847206c61
Philipp von Böselager <philipp.boeselager@web.de>
2017-05-02 17:50:44 +0200

    Fix Python handling in CalendarImporter (adds Resource default constructor)

--- app/services/imports/CalendarImporter.java
+++ app/services/imports/CalendarImporter.java
@@ -1,5 +1,6 @@
 package services.imports;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import models.Resource;
 import org.python.core.PyObject;
@@ -9,7 +10,6 @@ import org.python.util.PythonInterpreter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystem;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -21,6 +21,7 @@ public class CalendarImporter implements Importer{
   final private static String PATH_TO_PYTHON_LIBRARY;
   final private static String PATH_TO_PYTHON_IMPORTER;
   final private static String NAME_OF_PYTHON_IMPORTER;
+  final private static String PATH_AND_NAME_OF_PYTHON_IMPORTER;
 
   static{
     FileSystem fileSystem = new File(".").toPath().getFileSystem();
@@ -34,6 +35,8 @@ public class CalendarImporter implements Importer{
       .concat(SEP).concat("import")
       .concat(SEP).concat("iCal");
     NAME_OF_PYTHON_IMPORTER = "import_ical.py";
+    PATH_AND_NAME_OF_PYTHON_IMPORTER = PATH_TO_PYTHON_IMPORTER
+      .concat(SEP).concat(NAME_OF_PYTHON_IMPORTER);
   }
 
   @Override
@@ -44,16 +47,18 @@ public class CalendarImporter implements Importer{
   }
 
   public static List<Resource> importFromUrl(String aUrl, String aLanguage, String aApiKey) throws IOException {
+    PythonInterpreter.initialize(System.getProperties(), System.getProperties(), new String[0]);
     PythonInterpreter interpreter = new PythonInterpreter();
     interpreter.exec("import sys");
     interpreter.exec("sys.path.append('".concat(PATH_TO_PYTHON_IMPORTER).concat("')"));
     interpreter.exec("sys.path.append('".concat(PATH_TO_PYTHON_LIBRARY).concat("')"));
-    interpreter.exec("import ".concat(NAME_OF_PYTHON_IMPORTER));
+    interpreter.set("__file__", PATH_TO_PYTHON_IMPORTER);
+    interpreter.execfile(PATH_AND_NAME_OF_PYTHON_IMPORTER);
     PyObject importIcalPy = interpreter.get("import_ical");
     PyObject pythonResult = importIcalPy.__call__(new PyString(aUrl), new PyString(aLanguage), new PyString(aApiKey));
     String jsonArray = (String) pythonResult.__tojava__(String.class);
     ObjectMapper mapper = new ObjectMapper();
-    List<Resource> result = Arrays.asList(mapper.readValue(jsonArray, Resource[].class));
+    List<Resource> result = mapper.readValue(jsonArray, new TypeReference<List<Resource>>() {});
     return result;
   }
 }
