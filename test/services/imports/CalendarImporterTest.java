64d3a0c318b77044880a1c127832c45d6cc13c1a
Philipp von Böselager <philipp.boeselager@web.de>
2017-05-04 15:37:11 +0200

    Use mapzen-apikey in CalendarImporterTest

--- test/services/imports/CalendarImporterTest.java
+++ test/services/imports/CalendarImporterTest.java
@@ -1,8 +1,11 @@
 package services.imports;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import helpers.JsonTest;
 import helpers.ResourceHelpers;
 import models.Resource;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
@@ -17,14 +20,22 @@ import static org.junit.Assert.assertEquals;
  */
 public class CalendarImporterTest implements JsonTest{
 
+  private static Config mConfig;
+  private static String mMapzenApiKey;
+
+  @BeforeClass
+  public static void setupResources() throws IOException {
+    mConfig = ConfigFactory.parseFile(new File("conf/test.conf")).resolve();
+    mMapzenApiKey = mConfig.getString("mapzen.apikey");
+  }
+
   @Test
   public void testMultipleEventsImport() throws IOException {
     List<Resource> expected = ResourceHelpers.getResourcesWithoutIds(ResourceHelpers.unwrapRecords(
       getResourcesFromPagedCollectionFile("CalendarImporterTest/testMultipleEventsImport.OUT.1.json").getItems()));
     List<Resource> imported = ResourceHelpers.getResourcesWithoutIds(CalendarImporter.importFromUrl(
       "file://" + new File(".").getAbsolutePath() +
-      "/test/resources/CalendarImporterTest/testMultipleEventsImport.IN.1.ical", "de", "<api key goes here>"));
-      // TODO: fill mapzen API key (third parameter)
+      "/test/resources/CalendarImporterTest/testMultipleEventsImport.IN.1.ical", "de", mMapzenApiKey));
     Collections.sort(expected);
     Collections.sort(imported);
     assertEquals(expected.toString().trim(), imported.toString().trim());
