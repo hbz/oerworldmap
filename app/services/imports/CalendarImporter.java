0d11e590715c9cc7a8c1036305c178b04fd3267d
Philipp von Böselager <philipp.boeselager@web.de>
2017-05-09 15:19:48 +0200

    Refactor Python access in CalendarImporter

--- app/services/imports/CalendarImporter.java
+++ app/services/imports/CalendarImporter.java
@@ -4,13 +4,15 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.typesafe.config.Config;
 import models.Resource;
-import org.python.core.PyObject;
-import org.python.core.PyString;
 import org.python.util.PythonInterpreter;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 /**
@@ -53,24 +55,22 @@ public class CalendarImporter implements Importer{
   }
 
   private static List<Resource> importFromUrl(String aUrl, String aLanguage, String aApiKey) throws IOException {
-    PythonInterpreter.initialize(System.getProperties(), System.getProperties(), new String[0]);
-    PythonInterpreter interpreter = new PythonInterpreter();
+    String[] arguments = {PATH_AND_NAME_OF_PYTHON_IMPORTER, "DUMMY", aUrl, aLanguage, "./import/iCal/cache/temp.json", aApiKey};
+    PythonInterpreter.initialize(System.getProperties(), System.getProperties(), arguments);
+    PythonInterpreter interpreter = new org.python.util.PythonInterpreter();
     interpreter.exec("import sys");
     interpreter.exec("sys.path.append('".concat(PATH_TO_PYTHON_IMPORTER).concat("')"));
     interpreter.exec("sys.path.append('".concat(PATH_TO_PYTHON_LIBRARY).concat("')"));
     interpreter.set("__file__", PATH_TO_PYTHON_IMPORTER);
-    interpreter.exec("sys.argv.append('".concat("import.iCal.import_ical").concat("')"));
-    interpreter.exec("sys.argv.append('".concat("./import/iCal/import_ical.py").concat("')"));
-    interpreter.exec("sys.argv.append('".concat(aUrl).concat("')"));
-    interpreter.exec("sys.argv.append('".concat(aLanguage).concat("')"));
-    interpreter.exec("sys.argv.append('".concat("./import/iCal/cache/temp.json").concat("')"));
-    interpreter.exec("sys.argv.append('".concat(aApiKey).concat("')"));
+    interpreter.setOut(new StringWriter());
     interpreter.execfile(PATH_AND_NAME_OF_PYTHON_IMPORTER);
-    PyObject importIcalPy = interpreter.get("import_ical");
-    PyObject pythonResult = importIcalPy.__call__(new PyString(aUrl), new PyString(aLanguage), new PyString(aApiKey));
-    String jsonArray = (String) pythonResult.__tojava__(String.class);
     ObjectMapper mapper = new ObjectMapper();
-    List<Resource> result = mapper.readValue(jsonArray, new TypeReference<List<Resource>>() {});
+    String json = new String(Files.readAllBytes(Paths.get("./import/iCal/cache/temp.json")), StandardCharsets.UTF_8);
+    while (json.startsWith("[[") && json.endsWith("]]")){
+      json = json.substring(1, json.length()-1);
+    }
+    List<Resource> result = mapper.readValue(json, new TypeReference<List<Resource>>() {});
     return result;
   }
+
 }
