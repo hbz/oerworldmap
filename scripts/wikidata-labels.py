6897ff8f1c4ab695722bb61c6afd8fab933f38fe
Felix Ostrowski <felix.ostrowski@googlemail.com>
2017-07-05 11:41:06 +0200

    Generalize script to fetch labels from wikidata
    Usage:
        $ python scripts/wikidata-labels.py {wikidata-property} {language}

--- scripts/wikidata-labels.py
+++ scripts/wikidata-labels.py
@@ -3,13 +3,16 @@ import requests, sys, json
 query = '''
 PREFIX wdt: <http://www.wikidata.org/prop/direct/>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX p: <http://www.wikidata.org/prop/>
+PREFIX pq: <http://www.wikidata.org/prop/qualifier/>
 
-SELECT ?code ?label WHERE {
-  ?item wdt:P300 ?code .
+SELECT DISTINCT ?code ?label WHERE {
+  ?item wdt:%s ?code .
+  FILTER NOT EXISTS { ?item p:%s [ pq:P582 ?end ] . }
   ?item rdfs:label ?label .
   FILTER (LANG(?label) = "%s")
-}
-''' % sys.argv[1]
+} ORDER BY ASC(?code)
+''' % (sys.argv[1], sys.argv[1], sys.argv[2])
 
 url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
 data = requests.get(url, params={'query': query, 'format': 'json'}).json()
