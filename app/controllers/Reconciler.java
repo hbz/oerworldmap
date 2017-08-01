b1c7d826aa1c78543719ce38c02dff39910c5f32
Felix Ostrowski <felix.ostrowski@googlemail.com>
2017-08-01 15:43:40 +0200

    Use single reconcile action method

--- app/controllers/Reconciler.java
+++ app/controllers/Reconciler.java
@@ -2,7 +2,9 @@ package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.inject.Inject;
 import models.Resource;
+import org.apache.commons.lang3.StringUtils;
 import play.Configuration;
 import play.Environment;
 import play.libs.Json;
@@ -14,29 +16,28 @@ import services.QueryContext;
  */
 public class Reconciler extends OERWorldMap{
 
+  @Inject
   public Reconciler(Configuration aConf, Environment aEnv) {
     super(aConf, aEnv);
   }
 
 
-  public static Result meta(String callback) {
-    ObjectNode result = Json.newObject();
-    result.put("name", "oerworldmap reconciliation");
-    result.put("identifierSpace", "https://oerworldmap.org/resource");
-    result.put("schemaSpace", "https://oerworldmap.org/resource");
-    result.set("defaultTypes", Json.toJson(Resource.mIdentifiedTypes));
-    result.set("view", Json.newObject().put("url", "https://oerworldmap.org/resource/{{id}}"));
-    return callback.isEmpty() ?
-      ok(result) :
-      ok(String.format("/**/%s(%s);", callback, result.toString())).as("application/json");
-  }
-
-
   public Result reconcile(String aQueryTerm) {
-    QueryContext queryContext = getQueryContext();
-    final JsonNode reconciled = mBaseRepository.reconcile(aQueryTerm, 0, 10, null, null, queryContext);
-    // TODO: parametrize dynamically
-    return ok(reconciled);
+
+    if (StringUtils.isEmpty(aQueryTerm)) {
+      ObjectNode result = Json.newObject();
+      result.put("name", "oerworldmap reconciliation");
+      result.put("identifierSpace", "https://oerworldmap.org/resource");
+      result.put("schemaSpace", "https://oerworldmap.org/resource");
+      result.set("defaultTypes", Json.toJson(Resource.mIdentifiedTypes));
+      result.set("view", Json.newObject().put("url", "https://oerworldmap.org/resource/{{id}}"));
+      return ok(result);
+    } else {
+      QueryContext queryContext = getQueryContext();
+      final JsonNode reconciled = mBaseRepository.reconcile(aQueryTerm, 0, 10, null, null, queryContext);
+      // TODO: parametrize dynamically
+      return ok(reconciled);
+    }
   }
 
 }
