59edb9a2ef385666fa39342779078b1e23594818
Felix Ostrowski <felix.ostrowski@googlemail.com>
2017-08-15 11:04:04 +0200

    Remove superfluous methods

--- app/controllers/OERWorldMap.java
+++ app/controllers/OERWorldMap.java
@@ -6,7 +6,6 @@ import com.maxmind.geoip2.DatabaseReader;
 import helpers.JSONForm;
 import models.Resource;
 import models.TripleCommit;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import play.Configuration;
 import play.Environment;
@@ -21,28 +20,18 @@ import services.repository.ElasticsearchRepository;
 
 import javax.inject.Inject;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.InetAddress;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLDecoder;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Base64;
-import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
-import java.util.Set;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
 /**
  * @author fo
@@ -209,44 +198,6 @@ public abstract class OERWorldMap extends Controller {
 
   }
 
-  //TODO: is this right here? how else to implement?
-  public String getLabel(String aId) {
-
-    Resource resource = mBaseRepository.getResource(aId);
-    if (null == resource) {
-      return aId;
-    }
-    Object name = resource.get("name");
-    if (name instanceof ArrayList) {
-      // Return requested language
-      for (Object n : ((ArrayList) name)) {
-        if (n instanceof Resource) {
-          String language = ((Resource) n).getAsString("@language");
-          if (language.equals(getLocale().getLanguage())) {
-            return ((Resource) n).getAsString("@value");
-          }
-        }
-      }
-      // Return English if requested language is not available
-      for (Object n : ((ArrayList) name)) {
-        if (n instanceof Resource) {
-          String language = ((Resource) n).getAsString("@language");
-          if (language.equals("en")) {
-            return ((Resource) n).getAsString("@value");
-          }
-        }
-      }
-      // Return first available if English is not available
-      for (Object n : ((ArrayList) name)) {
-        if (n instanceof Resource) {
-          return ((Resource) n).getAsString("@value");
-        }
-      }
-    }
-
-    return aId;
-  }
-
   protected BaseRepository getRepository() {
     return mBaseRepository;
   }
@@ -283,95 +234,6 @@ public abstract class OERWorldMap extends Controller {
     }
     metadata.put(TripleCommit.Header.DATE_HEADER, ZonedDateTime.now().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));
     return metadata;
-
-  }
-
-
-  private String getClientTemplates() {
-
-    final List<String> templates = new ArrayList<>();
-    final String dir = "public/mustache/ClientTemplates/";
-    final ClassLoader classLoader = mEnv.classLoader();
-
-    String[] paths = new String[0];
-    try {
-      paths = getResourceListing(dir, classLoader);
-    } catch (URISyntaxException | IOException e) {
-      Logger.error("Could not find client templates", e);
-    }
-
-    for (String path : paths) {
-      try {
-        String template = "<script id=\"".concat(path).concat("\" type=\"text/mustache\">\n");
-        InputStream templateStream = classLoader.getResourceAsStream(dir + path);
-        if (templateStream == null){
-          templateStream = new FileInputStream(dir + path);
-        }
-        template = template.concat(IOUtils.toString(templateStream));
-        templateStream.close();
-        template = template.concat("</script>\n\n");
-        templates.add(template);
-      } catch (IOException e) {
-        Logger.error("Could not load client template", e);
-      }
-    }
-
-    return String.join("\n", templates);
-
-  }
-
-  /**
-   * List directory contents for a resource folder. Not recursive. This is
-   * basically a brute-force implementation. Works for regular files and also
-   * JARs.
-   *
-   * Adapted from http://www.uofr.net/~greg/java/get-resource-listing.html
-   *
-   * @param path
-   *          Should end with "/", but not start with one.
-   * @return Just the name of each member item, not the full paths.
-   * @throws URISyntaxException
-   * @throws IOException
-   */
-  private String[] getResourceListing(String path, ClassLoader classLoader)
-      throws URISyntaxException, IOException {
-
-    URL dirURL = classLoader.getResource(path);
-
-    if (dirURL == null) {
-      return mEnv.getFile(path).list();
-    } else if (dirURL.getProtocol().equals("jar")) {
-      String jarPath = dirURL.getPath().substring(5, dirURL.getPath().indexOf("!")); // strip
-                                                                                     // out
-                                                                                     // only
-                                                                                     // the
-                                                                                     // JAR
-                                                                                     // file
-      JarFile jar = new JarFile(URLDecoder.decode(jarPath, "UTF-8"));
-      Enumeration<JarEntry> entries = jar.entries(); // gives ALL entries in jar
-      Set<String> result = new HashSet<>(); // avoid duplicates in case it
-                                                  // is a subdirectory
-      while (entries.hasMoreElements()) {
-        String name = entries.nextElement().getName();
-        if (name.startsWith(path)) { // filter according to the path
-          String entry = name.substring(path.length());
-          int checkSubdir = entry.indexOf("/");
-          if (checkSubdir >= 0) {
-            // if it is a subdirectory, we just return the directory name
-            entry = entry.substring(0, checkSubdir);
-          }
-          if (!entry.equals("")) {
-            result.add(entry);
-          }
-        }
-      }
-      jar.close();
-      return result.toArray(new String[result.size()]);
-    } else if (dirURL.getProtocol().equals("file")) {
-      return new File(dirURL.getFile()).list();
-    }
-
-    throw new UnsupportedOperationException("Cannot list files for URL " + dirURL);
 
   }
 
