36de941fe8674fc33cb6d139c9ecb3f4b5f4ef31
Philipp von Böselager <philipp.boeselager@web.de>
2018-02-27 14:10:41 +0100

    Replace IndicesClient.exists() by custom function until Elasticsearch v6.3 is used

--- test/helpers/ElasticsearchHelpers.java
+++ test/helpers/ElasticsearchHelpers.java
@@ -2,10 +2,12 @@ package helpers;
 
 import services.repository.ElasticsearchRepository;
 
+import java.io.IOException;
+
 public class ElasticsearchHelpers {
 
   // create a new clean ElasticsearchIndex for this Test class
-  public static void cleanIndex(ElasticsearchRepository aEsRepo, String aIndex) {
+  public static void cleanIndex(ElasticsearchRepository aEsRepo, String aIndex) throws IOException {
     if (aEsRepo.hasIndex(aIndex)) {
       aEsRepo.deleteIndex(aIndex);
     }
