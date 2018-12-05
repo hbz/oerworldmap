e3ab8b32993981854e1067253e4a065e134fee20
Felix Ostrowski <felix.ostrowski@googlemail.com>
2018-12-05 17:52:06 +0100

    Use native Jena JSON-LD framing

--- app/services/ResourceFramer.java
+++ app/services/ResourceFramer.java
@@ -3,9 +3,13 @@ package services;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import helpers.JsonLdConstants;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import models.Resource;
 import org.apache.commons.io.IOUtils;
 import org.apache.jena.query.DatasetFactory;
@@ -22,20 +26,11 @@ import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
 import org.apache.jena.riot.WriterDatasetRIOT;
+import org.apache.jena.riot.system.PrefixMap;
 import org.apache.jena.riot.system.RiotLib;
-import org.apache.jena.shared.Lock;
 import org.apache.jena.sparql.core.DatasetGraph;
 import org.apache.jena.vocabulary.RDF;
 import play.Logger;
-import services.repository.TriplestoreRepository;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * Created by fo on 23.03.16.
@@ -50,137 +45,51 @@ public class ResourceFramer {
     mContextUrl = aContextUrl;
   }
 
+  public static void write(Model m) {
+    DatasetGraph g = DatasetFactory.create(m).asDatasetGraph();
+    WriterDatasetRIOT w = RDFDataMgr.createDatasetWriter(RDFFormat.JSONLD_FRAME_PRETTY);
+    PrefixMap pm = RiotLib.prefixMap(g);
+    JsonLDWriteContext context = new JsonLDWriteContext();
+    context.setJsonLDContext("{\"@context\":\"https://oerworldmap.org/assets/json/context.json\"}");
+    context.setFrame("{\"@context\":\"https://oerworldmap.org/assets/json/context.json\", \"@type\": \"http://schema.org/Action\", \"@embed\": \"@always\"}");
+    w.write(System.out, g, pm, null, context) ;
+  }
+
   public static Resource resourceFromModel(Model aModel, String aId) throws IOException {
 
-    String describeStatement = String.format(TriplestoreRepository.EXTENDED_DESCRIPTION, aId);
-    Model dbstate = ModelFactory.createDefaultModel();
+    //write(aModel);
 
-    aModel.enterCriticalSection(Lock.READ);
-    dbstate.enterCriticalSection(Lock.WRITE);
-    try {
-      try (QueryExecution queryExecution = QueryExecutionFactory
-        .create(QueryFactory.create(describeStatement),
-          aModel)) {
-        queryExecution.execDescribe(dbstate);
+    NodeIterator types = aModel.listObjectsOfProperty(aModel.createResource(aId), RDF.type);
 
-        ByteArrayOutputStream unframed = new ByteArrayOutputStream();
-        RDFDataMgr.write(unframed, dbstate, RDFFormat.JSONLD_EXPAND_PRETTY);
-        unframed.close();
-
-        NodeIterator types = aModel.listObjectsOfProperty(aModel.createResource(aId), RDF.type);
-
-        if (types.hasNext()) {
-          DatasetGraph g = DatasetFactory.create(dbstate).asDatasetGraph();
-          JsonLDWriteContext ctx = new JsonLDWriteContext();
-          String context = String.format("{ \"@context\": \"%s\"}", mContextUrl);
-          ctx.setJsonLDContext(context);
-          ByteArrayOutputStream boas = new ByteArrayOutputStream();
-          WriterDatasetRIOT w = RDFDataMgr.createDatasetWriter(RDFFormat.JSONLD_COMPACT_PRETTY);
-          w.write(boas, g, RiotLib.prefixMap(g), null, ctx);
-          JsonNode jsonNode = mObjectMapper.readTree(boas.toByteArray());
-          if (jsonNode.has(JsonLdConstants.GRAPH)) {
-            ArrayNode graphs = (ArrayNode) jsonNode.get(JsonLdConstants.GRAPH);
-            for (JsonNode graph : graphs) {
-              if (graph.get(JsonLdConstants.ID).asText().equals(aId)) {
-                ObjectNode result = (ObjectNode) buildTree(graph, graphs);
-                result.put(JsonLdConstants.CONTEXT, mContextUrl);
-                Logger.debug("Framed " + aId);
-                return Resource.fromJson(result);
-              }
-            }
-          } else {
-            ObjectNode result = (ObjectNode) jsonNode;
+    if (types.hasNext()) {
+      String type = types.next().toString();
+      DatasetGraph g = DatasetFactory.create(aModel).asDatasetGraph();
+      JsonLDWriteContext ctx = new JsonLDWriteContext();
+      String context = String.format("{ \"@context\": \"%s\"}", mContextUrl);
+      ctx.setJsonLDContext(context);
+      ctx.setFrame("{\"@context\":\"https://oerworldmap.org/assets/json/context.json\", \"@embed\": \"@always\", \"@type\": \"" + type + "\"}");
+      ByteArrayOutputStream boas = new ByteArrayOutputStream();
+      WriterDatasetRIOT w = RDFDataMgr.createDatasetWriter(RDFFormat.JSONLD_FRAME_PRETTY);
+      w.write(boas, g, RiotLib.prefixMap(g), null, ctx);
+      JsonNode jsonNode = mObjectMapper.readTree(boas.toByteArray());
+      if (jsonNode.has(JsonLdConstants.GRAPH)) {
+        ArrayNode graphs = (ArrayNode) jsonNode.get(JsonLdConstants.GRAPH);
+        for (JsonNode graph : graphs) {
+          if (graph.get(JsonLdConstants.ID).asText().equals(aId)) {
+            ObjectNode result = (ObjectNode) graph;
             result.put(JsonLdConstants.CONTEXT, mContextUrl);
             Logger.debug("Framed " + aId);
             return Resource.fromJson(result);
           }
         }
+      } else {
+        ObjectNode result = (ObjectNode) jsonNode;
+        result.put(JsonLdConstants.CONTEXT, mContextUrl);
+        Logger.debug("Framed " + aId);
+        return Resource.fromJson(result);
       }
-    } finally {
-      dbstate.leaveCriticalSection();
-      aModel.leaveCriticalSection();
     }
 
-    return null;
-  }
-
-  private static JsonNode buildTree(JsonNode graph, ArrayNode graphs) {
-    if (graph.isArray()) {
-      ArrayNode result = new ArrayNode(JsonNodeFactory.instance);
-      Iterator<JsonNode> elements = graph.elements();
-      while (elements.hasNext()) {
-        JsonNode element = elements.next();
-        result.add(buildTree(element, graphs));
-      }
-      return result;
-    } else if (graph.isObject()) {
-      ObjectNode result = new ObjectNode(JsonNodeFactory.instance);
-      Iterator<String> fieldNames = graph.fieldNames();
-      while (fieldNames.hasNext()) {
-        String fieldName = fieldNames.next();
-        if (!(fieldName.equals(JsonLdConstants.ID) && graph.get(fieldName).asText()
-          .startsWith("_:"))) {
-          result.set(fieldName, link(graph.get(fieldName), graphs));
-        }
-      }
-      return result;
-    } else {
-      return graph;
-    }
-  }
-
-  private static JsonNode link(JsonNode ref, ArrayNode graphs) {
-    JsonNode graph;
-    if (ref.has(JsonLdConstants.ID)) {
-      graph = getGraph(ref.get(JsonLdConstants.ID).asText(), graphs);
-      if (graph == null) {
-        graph = ref;
-      }
-    } else {
-      graph = ref;
-    }
-    List<String> linkProperties = Arrays
-      .asList("@id", "@type", "@value", "@language", "name", "image", "location",
-        "startDate", "endDate", "agent", "object", "description", "text", "comment", "author",
-        "dateCreated", "startTime");
-    if (graph.isArray()) {
-      ArrayNode result = new ArrayNode(JsonNodeFactory.instance);
-      Iterator<JsonNode> elements = graph.elements();
-      while (elements.hasNext()) {
-        JsonNode element = elements.next();
-        result.add(link(element, graphs));
-      }
-      return result;
-    } else if (graph.isObject()) {
-      if (graph.get(JsonLdConstants.ID) != null && graph.get(JsonLdConstants.ID).asText()
-        .startsWith("_:")) {
-        return buildTree(graph, graphs);
-      }
-      ObjectNode result = new ObjectNode(JsonNodeFactory.instance);
-      Iterator<String> fieldNames = graph.fieldNames();
-      while (fieldNames.hasNext()) {
-        String fieldName = fieldNames.next();
-        if (linkProperties.contains(fieldName)) {
-          JsonNode value = graph.get(fieldName);
-          if (value.isObject() || value.isArray()) {
-            result.set(fieldName, link(value, graphs));
-          } else {
-            result.set(fieldName, value);
-          }
-        }
-      }
-      return result;
-    } else {
-      return graph;
-    }
-  }
-
-  private static JsonNode getGraph(String aId, ArrayNode graphs) {
-    for (JsonNode graph : graphs) {
-      if (graph.get(JsonLdConstants.ID).asText().equals(aId)) {
-        return graph;
-      }
-    }
     return null;
   }
 
