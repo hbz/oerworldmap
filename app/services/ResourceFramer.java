42457829f11c7ac4209607662cf2fbc60cf242b0
Felix Ostrowski <felix.ostrowski@googlemail.com>
2018-12-20 11:30:22 +0100

    Frame by explicit reference instead of type

--- app/services/ResourceFramer.java
+++ app/services/ResourceFramer.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.jsonldjava.core.JsonLdOptions;
-import helpers.JsonLdConstants;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -15,6 +14,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import models.Resource;
 import org.apache.commons.io.IOUtils;
@@ -26,7 +26,7 @@ import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.NodeIterator;
+import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.riot.JsonLDWriteContext;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
@@ -34,8 +34,6 @@ import org.apache.jena.riot.RDFFormat;
 import org.apache.jena.riot.WriterDatasetRIOT;
 import org.apache.jena.riot.system.RiotLib;
 import org.apache.jena.sparql.core.DatasetGraph;
-import org.apache.jena.vocabulary.RDF;
-import play.Logger;
 
 /**
  * Created by fo on 23.03.16.
@@ -44,15 +42,20 @@ public class ResourceFramer {
 
   private static final ObjectMapper mObjectMapper = new ObjectMapper();
 
+  private static final org.apache.jena.rdf.model.Resource recordId = ResourceFactory
+    .createResource("urn:uuid:".concat(UUID.randomUUID().toString()));
+
+  private static final org.apache.jena.rdf.model.Property resourceLink = ResourceFactory
+    .createProperty("urn:uuid:".concat(UUID.randomUUID().toString()));
+
   private static WriterDatasetRIOT mWriter = RDFDataMgr.createDatasetWriter(RDFFormat.JSONLD_FRAME_PRETTY);
 
   public static Resource resourceFromModel(Model aModel, String aId, String aContextUrl) throws IOException {
 
-    NodeIterator types = aModel.listObjectsOfProperty(aModel.createResource(aId), RDF.type);
-
-    if (!types.hasNext()) {
-      return null;
-    }
+    // Create "record" that points at the id of the resource
+    Model record = ModelFactory.createDefaultModel();
+    record.add(aModel);
+    record.add(recordId, resourceLink, ResourceFactory.createResource(aId));
 
     // JSON-LD context
     Map<String, String> jsonLdContext = new HashMap<>();
@@ -62,12 +65,12 @@ public class ResourceFramer {
     Map<String, String> frame = new HashMap<>();
     frame.put("@context", aContextUrl);
     frame.put("@embed", "@always");
-    frame.put("@type", types.next().toString());
+    frame.put(resourceLink.toString(), "{}");
 
     // Jena write context config
     JsonLdOptions jsonLdOptions = new JsonLdOptions();
     jsonLdOptions.setUseNativeTypes(true);
-    jsonLdOptions.setOmitGraph(false);
+    jsonLdOptions.setOmitGraph(true);
     JsonLDWriteContext jenaWriteContext = new JsonLDWriteContext();
     jenaWriteContext.setJsonLDContext(jsonLdContext);
     jenaWriteContext.setFrame(frame);
@@ -75,20 +78,11 @@ public class ResourceFramer {
 
     // Serialize to JSON-LD and deserialize to JsonNode
     ByteArrayOutputStream boas = new ByteArrayOutputStream();
-    DatasetGraph g = DatasetFactory.create(aModel).asDatasetGraph();
+    DatasetGraph g = DatasetFactory.create(record).asDatasetGraph();
     mWriter.write(boas, g, RiotLib.prefixMap(g), null, jenaWriteContext);
     JsonNode jsonNode = mObjectMapper.readTree(boas.toByteArray());
-    ArrayNode graphs = (ArrayNode) jsonNode.get(JsonLdConstants.GRAPH);
-    for (JsonNode graph : graphs) {
-      if (graph.get(JsonLdConstants.ID).asText().equals(aId)) {
-        ObjectNode result = (ObjectNode) graph;
-        result.put(JsonLdConstants.CONTEXT, aContextUrl);
-        Logger.debug("Framed " + aId);
-        return Resource.fromJson(prune(result));
-      }
-    }
 
-    return null;
+    return Resource.fromJson(jsonNode.get(resourceLink.toString()));
   }
 
   private static ObjectNode prune(ObjectNode node) {
@@ -124,13 +118,12 @@ public class ResourceFramer {
   }
 
   public static List<Resource> flatten(Resource aResource, String aContextUrl) throws IOException {
-
     Model model = ModelFactory.createDefaultModel();
+    List<Resource> resources = new ArrayList<>();
+    String subjectsQuery = "SELECT DISTINCT ?s WHERE { ?s ?p ?o . FILTER isIRI(?s) }";
+
     RDFDataMgr
       .read(model, IOUtils.toInputStream(aResource.toString(), StandardCharsets.UTF_8), Lang.JSONLD);
-    List<Resource> resources = new ArrayList<>();
-
-    String subjectsQuery = "SELECT DISTINCT ?s WHERE { ?s ?p ?o . FILTER isIRI(?s) }";
     try (QueryExecution queryExecution = QueryExecutionFactory
       .create(QueryFactory.create(subjectsQuery), model)) {
       ResultSet resultSet = queryExecution.execSelect();
