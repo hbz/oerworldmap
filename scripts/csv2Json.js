672d934f2f8d481657660e7683ee420c4763848c
Felix Ostrowski <felix.ostrowski@googlemail.com>
2019-06-24 17:42:32 +0200

    Simplify CSV conversion script

--- scripts/csv2Json.js
+++ scripts/csv2Json.js
@@ -2,21 +2,12 @@ const csv = require('csv-parser')
 const pointer = require('json-pointer')
 
 const items = []
-let isHeader = true
-const headers = []
-process.stdin.pipe(csv({headers: false})).on('data', function(row) {
+process.stdin.pipe(csv()).on('data', function(row) {
   try {
-    if (isHeader) {
-      for (const [key, value] of Object.entries(row)) {
-        headers[key] = value
-      }
-      isHeader = false
-      return
-    }
     item = {}
     for (const [key, value] of Object.entries(row)) {
       if (!!value)
-        pointer.set(item, headers[key], value)
+        pointer.set(item, key, value)
     }
     items.push(item)
   } catch (err) {
