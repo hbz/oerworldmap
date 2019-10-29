3709defdec27b79bf9c27654dfd9ab3c831dd2ae
Felix Ostrowski <felix.ostrowski@googlemail.com>
2019-10-29 15:19:49 +0100

    Add startup script for systemd usage

---
+++ startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd $(dirname $(readlink -f $0)) || exit
+
+until curl --output /dev/null --silent --head --fail http://localhost:9200; do
+    printf '.'
+    sleep 5
+done
+until curl --output /dev/null --silent --head --fail http://localhost:8080; do
+    printf '.'
+    sleep 5
+done
+
+./target/universal/stage/bin/oerworldmap -J-Xms2G -J-Xmx2G -J-server -Duser.language=en -Duser.country=EN -Dconfig.file=./conf/application.oerworldmap.conf
